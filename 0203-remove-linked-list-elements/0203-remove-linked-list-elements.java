6819de8abea35d3c82cd7539babfd34ad94688cc
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-06-27 23:51:09 -0700

    Time: 1 ms (81.41%), Space: 44.9 MB (40.18%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,17 +10,22 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null) return null;
-        ListNode dummy = new ListNode(0, head), p = dummy, pre = p;
+        if(head == null) return null;
+        ListNode p = head, pre = head;
         while (p != null) {
-            if (p.val == val) {
+            if(p.val == val && p == head) {
+                head = head.next;
+                p = head;
+                continue;
+            }
+            if(p.val == val) {
                 pre.next = p.next;
+                p = p.next;
             } else {
                 pre = p;
+                p = p.next;
             }
-            p = p.next;
         }
-        head = dummy.next;
         return head;
     }
 }
