02c0ab2a35b80ad84f462fda33c0f0e477d01426
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-06-28 22:13:18 -0700

    Time: 0 ms (100.00%), Space: 43.9 MB (42.99%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // has a cycle
+            if (slow == fast) {
+                // record the position where they meet in a cycle
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        // do not have a cycle
+        return null;
+    }
+    
+}
