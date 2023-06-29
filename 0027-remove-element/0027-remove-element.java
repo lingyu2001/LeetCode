4611f661085fc0371b54594df9062bc750d71c98
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-06-29 11:53:56 -0700

    Time: 0 ms (100.00%), Space: 41.3 MB (28.74%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,33 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         // int cnt = nums.length;
-        int i = 0, j = nums.length - 1;
-        while (i < j) {
-            if (nums[i] == val && nums[j] != val) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
             }
-            // i points at positions with val value
-            if (nums[i] != val) {
-                i++;
-            }
-            // j points at positions with non val value
-            if (nums[j] == val) {
-                j--;
-            }
+            j++;
         }
-        // if (i == j && nums[i] == val) {
-        //     return cnt - 1;
-        // }
-        int cnt = 0;
-        for (int k = 0; k < nums.length; k++) {
-            if (nums[k] == val) {
-                break;
-            } else {
-                cnt++;
-            }
-        }
-        return cnt;
+        return i;
     }
 }
