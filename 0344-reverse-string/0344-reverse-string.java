a8ffd14efa45e59ea120a687a0c286b01d45b6ea
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-06-29 11:58:28 -0700

    Time: 1 ms (86.22%), Space: 50.9 MB (41.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public void reverseString(char[] s) {
-        for(int i = 0, j = s.length - 1; i < j; i++,j--) {
+        for(int i = 0, j = s.length - 1; i < s.length / 2; i++,j--) {
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
