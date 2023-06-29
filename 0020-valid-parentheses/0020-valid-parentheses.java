4cdde6c93d83c2bdfd62e05375f274172a56d9ca
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-06-29 16:55:40 -0700

    Time: 2 ms (91.66%), Space: 40.5 MB (87.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        Map<Character,Character> map = new HashMap<>();
-        map.put('(',')');
-        map.put('{','}');
-        map.put('[',']');
+        char[] chars = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-        if (s.length() % 2 == 1) return false;
-        for (int i = 0; i < s.length(); i++) {
-            // ),] }: pop an element and see if the element can match the character
-            if (!map.containsKey(s.charAt(i))) {
-                if (stack.empty()) return false;
-                char c = stack.pop();
-                // do not match
-                if (map.get(c) != s.charAt(i)) return false;
+        for (int i = 0; i < chars.length; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
             } else {
-                // (,{,[: add to the stack
-                stack.push(s.charAt(i));
+                char top = ' ';
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    top = stack.pop();
+                }
+                if (c == ']' && top != '[') {
+                    return false;
+                } else if (c == '}' && top != '{') {
+                    return false;
+                } else if (c == ')' && top != '(') {
+                    return false;
+                }
             }
         }
-        if (stack.isEmpty())
-            return true;
-        return false;
+        return stack.isEmpty();
     }
 }
