7692062e8fcb5fdbceb57b89a1cd7dc724735619
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-06-29 16:26:53 -0700

    Time: 35 ms (95.90%), Space: 50.6 MB (70.47%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,35 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
-    LinkedList<Integer> list = new LinkedList<>();
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        int i = 0, left, right;
-        for(i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+	// 找出a + b + c = 0
+        // a = nums[i], b = nums[left], c = nums[right]
+        for (int i = 0; i < nums.length; i++) {
+	    // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+            if (nums[i] > 0) { 
+                return result;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {  // 去重a
                 continue;
             }
-            for(left = i + 1, right = nums.length - 1;left < right;) {
-                if(nums[i] + nums[left] + nums[right] == 0) {
-                    list.add(nums[i]);
-                    list.add(nums[left]);
-                    list.add(nums[right]);
-                    result.add(new ArrayList<>(list));
-                    list.clear();
-                    left++;
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (right > left) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
                     right--;
-                    while (left < right && right + 1 < nums.length && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-                } else if(nums[i] + nums[left] + nums[right] < 0) {
+                } else if (sum < 0) {
                     left++;
                 } else {
-                    right--;
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+		    // 去重逻辑应该放在找到一个三元组之后，对b 和 c去重
+                    while (right > left && nums[right] == nums[right - 1]) right--;
+                    while (right > left && nums[left] == nums[left + 1]) left++;
+                    
+                    right--; 
+                    left++;
                 }
             }
         }
