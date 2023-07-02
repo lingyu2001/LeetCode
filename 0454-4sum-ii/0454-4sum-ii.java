cabddf4a4839238881c8d8edb24151d29dce6827
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-02 15:11:02 -0700

    Time: 137 ms (56.52%), Space: 44.1 MB (31.40%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                map.put(nums1[i] + nums2[j], map.getOrDefault(nums1[i] + nums2[j], 0) + 1);
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < nums3.length; i++) {
+            for (int j = 0; j < nums4.length; j++) {
+                int find = -(nums3[i] + nums4[j]);
+                result += map.getOrDefault(find, 0);
+            }
+        }
+        return result;
+    }
+}
