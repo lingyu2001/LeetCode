a855c6bc2c156343fd051846535caa309f5e2239
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-02 17:23:47 -0700

    Time: 45 ms (59.61%), Space: 50.5 MB (78.19%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,37 +1,31 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-    LinkedList<Integer> list = new LinkedList<>();
+    List<Integer> current = new ArrayList<>();
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int i = 0, left, right;
-		// 对a去重
-        for(i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) 
-                return result;
+        // determine i
+        for (int i = 0; i < nums.length; i++) {
+            // distinct i
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            for(left = i + 1, right = nums.length - 1;left < right;) {
+            for (int left = i + 1, right = nums.length - 1; left < right; ) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0) {
-                    list.add(nums[i]);
-                    list.add(nums[left]);
-                    list.add(nums[right]);
-                    result.add(new ArrayList<>(list));
-                    list.clear();
+                if (sum == 0) {
+                    current.add(nums[i]);
+                    current.add(nums[left]);
+                    current.add(nums[right]);
+                    result.add(new ArrayList<>(current));
+                    current.clear();
                     left++;
-										// 对b和c去重
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
                     right--;
-                    while (left < right && right + 1 < nums.length && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-                } else if (sum < 0) {
-                    left++;
+                    // distinct b,c
+                    while (left < nums.length && nums[left] == nums[left - 1]) left++;
+                    while (right >= 0 && nums[right] == nums[right + 1]) right--;
+                } else if (sum > 0) {
+                    right--;
                 } else {
-                    right--;
+                    left++;
                 }
             }
         }
