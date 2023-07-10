79b8e71d4885ba56dd752df5dbfacea2aafed942
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-10 12:40:58 -0700

    Time: 0 ms (100.00%), Space: 44.1 MB (45.68%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -14,39 +14,10 @@
  * }
  */
 class Solution {
-    TreeNode root1;
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if (root == null) return new TreeNode(val);
-        root1 = root;
-        helper(root, val);
-        return root1;
-    }
-    
-    public void helper(TreeNode root, int val) {
-        if (root == null) {
-            return;
-        }
-        // insertion
-        if (root.left != null && root.right != null) {
-            if (root.val < val) helper(root.right, val);
-            else helper(root.left,val);
-        } else if (root.left == null && root.right == null) {
-            if (root.val < val) root.right = new TreeNode(val);
-            else root.left = new TreeNode(val);
-            return;
-        } else if (root.left == null && root.right != null) {
-            if (root.val < val) helper(root.right, val);
-            else {
-                root.left = new TreeNode(val);
-                return;
-            }
-        } else {
-            if (root.val < val) {
-                root.right = new TreeNode(val);
-                return;
-            } else {
-                helper(root.left, val);
-            }
-        }
+        if (root.val > val) root.left = insertIntoBST(root.left, val);
+        else root.right = insertIntoBST(root.right, val);
+        return root;
     }
 }
