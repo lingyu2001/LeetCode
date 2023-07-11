e479d14ba5dedd6013f4edac69c53542d9aa9d28
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-11 16:32:07 -0700

    Time: 2 ms (79.27%), Space: 44.8 MB (21.85%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int cover = 0;
+        if (nums.length == 1) return true;
+        for (int i = 0; i <= cover; i++) {
+            cover = Math.max(cover, i + nums[i]);
+            if (cover >= nums.length - 1) return true;
+        }
+        return false;
+    }
+}
