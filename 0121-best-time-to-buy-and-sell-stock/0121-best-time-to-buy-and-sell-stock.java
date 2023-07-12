83cedfae99b7922b536cc11a546dab7770d717a6
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-12 13:32:17 -0700

    Time: 1 ms (99.91%), Space: 61 MB (64.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // calculate the diff array: 
-        int[] diff = new int[prices.length - 1];
-        for (int i = 1; i < prices.length; i++) {
-            diff[i - 1] = prices[i] - prices[i - 1];
+        int max = 0;
+        int l = 0, r = 1; 
+        while (r < prices.length) {
+            int profit = prices[r] - prices[l];
+            if (profit > 0) {
+                max = Math.max(profit, max);
+            } else 
+                l = r;
+            r++;
         }
-        // iterate the diff array
-        int max = Integer.MIN_VALUE, sum = 0;
-        for (int i = 0; i < diff.length; i++) {
-            sum += diff[i];
-            if (sum > max) max = sum;
-            if (sum < 0) sum = 0;
-        }
-        if (max < 0) return 0;
         return max;
     }
 }
