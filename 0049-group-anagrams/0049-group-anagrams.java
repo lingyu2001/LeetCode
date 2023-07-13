243b3d08ac3da4d44d052a134803a375fb2daba8
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-13 12:35:33 -0700

    Time: 22 ms (26.13%), Space: 48.4 MB (11.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,27 @@
 class Solution {
-
+    //1207
+    List<List<String>> res = new ArrayList<>();
+    Map<String,List<String>> map =  new HashMap<>();
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        if (strs.length == 0) return res;
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            int[] hash = new int[26];
-            for (char c : s.toCharArray()) {
-                hash[c - 'a']++;
+        // iterate each str in strs
+        // for each str 
+            // calculate the hashValue of str(the count of the charatcers in the str)
+            // if the hash is not in the map --> create a key and add the str 
+            // if it is already in the map --> add the str to the list
+        // iterate the whole key in map and add all values in res;
+        for (String str : strs) {
+            int[] cnt = new int[26];
+            // count the value
+            for (char c : str.toCharArray()) {
+                cnt[c - 'a']++;
             }
-            String key = new String(Arrays.toString(hash));
-            map.computeIfAbsent(key, k -> new ArrayList<>());
-            map.get(key).add(s);
+            String key = Arrays.toString(cnt);
+            if (map.containsKey(key)) {
+                map.get(key).add(str);
+            } else {
+                map.put(key, new ArrayList<>());
+                map.get(key).add(str);
+            }
         }
         res.addAll(map.values());
         return res;
