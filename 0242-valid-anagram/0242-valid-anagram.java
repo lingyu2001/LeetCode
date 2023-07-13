6467a9ee8cb7a17380487e00239ddb320a9f535e
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-13 11:59:11 -0700

    Time: 4 ms (86.63%), Space: 41.9 MB (94.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
+        int[] ints = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i),0) + 1);
-            map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i),0) + 1);
+            ints[s.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < s.length(); i++) {
-            if (!map1.getOrDefault(s.charAt(i),0).equals(map2.getOrDefault(s.charAt(i),0)))
-                return false;
+        for (int i = 0; i < t.length(); i++) {
+            ints[t.charAt(i) - 'a']--;
+            if (ints[t.charAt(i) - 'a'] < 0) return false;
         }
         return true;
     }
