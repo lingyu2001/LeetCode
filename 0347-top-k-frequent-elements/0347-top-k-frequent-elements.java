6baf42933595bf21bf5c3aae588a0a2dd242abba
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-13 13:40:03 -0700

    Time: 14 ms (75.12%), Space: 48.1 MB (17.80%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,18 @@
 class Solution {
+    //1236
     public int[] topKFrequent(int[] nums, int k) {
-        // calculate the count of each element
-        Map<Integer, Integer> map = new HashMap<>(); 
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
         }
-        // sort the array according to their counts
-        //在优先队列中存储二元组(num,cnt),cnt表示元素值num在数组中的出现次数
-        //出现次数按从队头到队尾的顺序是从大到小排,出现次数最多的在队头(相当于大顶堆)
-        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair2[1] - pair1[1]);
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2)->pair2[1] - pair1[1]);
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
             pq.add(new int[]{entry.getKey(), entry.getValue()});
         }
-        // return the top k elements with the bigger counts
-        int[] ret = new int[k];
+        int[] res = new int[k];
         for (int i = 0; i < k; i++) {
-            ret[i] = pq.poll()[0];
+            res[i] = pq.poll()[0];
         }
-        return ret;
+        return res;
     }
 }
