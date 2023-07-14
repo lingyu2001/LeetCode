f8c93b8e5515ef19f8ca60a706086560b2673ab7
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-13 18:42:57 -0700

    Time: 2 ms (82.23%), Space: 45.6 MB (71.79%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,20 +1,12 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int max = 0, index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (i < index) arr[i] = arr[index];
-            else {
-                max = 0;
-                for (int j = index + 1; j < arr.length; j++) {
-                    if (max < arr[j]) {
-                        max = arr[j];
-                        index = j;
-                    }
-                }
-                arr[i] = arr[index];
-            }
-        }
+        int rightMax = arr[arr.length - 1] ;
         arr[arr.length - 1] = -1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+             int newEle = arr[i];
+             arr[i] = rightMax;
+             rightMax = Math.max(newEle, rightMax);
+        }
         return arr;
     }
 }
