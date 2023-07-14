92cda2bc130355a25ee69d1d2876e22624926384
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-14 11:58:48 -0700

    Time: 0 ms (100.00%), Space: 44.9 MB (65.40%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,14 +1,12 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // if (target < nums[0] || target > nums[nums.length - 1]) {
-        //     return -1;
-        // }
-        int left = 0, right = nums.length - 1, mid = 0;
-        while (left <= right) {
-            mid = left + (right - left) / 2;
+        if (target < nums[0] || target > nums[nums.length - 1]) return -1;
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
             else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            else right = mid;
         }
         return -1;
     }
