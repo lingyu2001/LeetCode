460e9388d087f99828a08554c45a7e773c29c700
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-15 15:12:08 -0700

    Time: 2 ms (86.02%), Space: 40.1 MB (24.78%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,15 +1,20 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int left = 1, right = n, max = 0;
+        int left = 1, right = n, res = 0;
         while (left <= right) {
-            long mid = left + (right - left) / 2;
-            long cnt = ((1 + mid) * mid)/ 2;
-            if (cnt == n) return (int)mid;
-            else if (cnt < n) {
-                left = (int)mid + 1;
-                max = Math.max(max, (int)mid);
-            }else right = (int)mid - 1;
+            int mid = left + (right - left) / 2;
+            long total = (long)((mid / 2.0) * (mid + 1));
+            if (total == n) {
+                return mid;
+            } else if (total < n) {
+                res = Math.max(res,mid);
+                left = mid + 1;
+            } else {
+                // 这里为什么是mid - 1；
+                // 这个不是查找某一个数，
+                right = mid - 1;
+            }
         }
-        return max;
+        return res;
     }
 }
