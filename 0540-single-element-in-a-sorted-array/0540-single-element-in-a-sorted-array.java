eaea85754a62bc11dfc49d6881de3a775aa33e68
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-15 17:50:47 -0700

    Time: 0 ms (100.00%), Space: 50.2 MB (51.43%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,26 +1,27 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int lo = 0;
-        int hi = nums.length - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            boolean halvesAreEven = (hi - mid) % 2 == 0;
-            if (nums[mid + 1] == nums[mid]) {
-                if (halvesAreEven) {
-                    lo = mid + 2;
+         int left = 0, right = nums.length - 1;
+         while (left < right) {
+            int mid = left + (right - left) / 2;
+            int rightSize = right - mid;
+            if (rightSize % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    left = mid + 2;
+                } else if (nums[mid] == nums[mid - 1]){
+                    right = mid - 2;
                 } else {
-                    hi = mid - 1;
-                }
-            } else if (nums[mid - 1] == nums[mid]) {
-                if (halvesAreEven) {
-                    hi = mid - 2;
-                } else {
-                    lo = mid + 1;
+                    return nums[mid];
                 }
             } else {
-                return nums[mid];
+                if (nums[mid] == nums[mid + 1]) {
+                    right = mid - 1;
+                } else if (nums[mid] == nums[mid - 1]) {
+                    left = mid + 1;
+                } else {
+                    return nums[mid];
+                }
             }
         }
-        return nums[lo];
+        return nums[left];
     }
 }
