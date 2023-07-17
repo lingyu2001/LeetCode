0eda7497828b28051ee507080f909bc54db5c5c4
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-17 10:36:59 -0700

    Time: 4 ms (79.50%), Space: 43.8 MB (26.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    public int[] nextGreaterElement1(int[] nums1, int[] nums2) {
         // sol1: bf
         int[] ans = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
@@ -17,6 +17,24 @@ class Solution {
                 }
                 if (j == nums2.length - 1 && res == -1) ans[i] = -1;
             }
+        }
+        return ans;
+    }
+    
+    // sol2
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < ans.length; i++) ans[i] = -1; 
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i],i);
+        }
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!s.isEmpty() && nums2[i] > s.peek()) {
+                ans[map.get(s.pop())] = nums2[i];
+            }
+            if (map.containsKey(nums2[i])) s.push(nums2[i]);
         }
         return ans;
     }
