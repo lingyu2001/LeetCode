47bc86ff2000a3101fa00e5f409be14d15920a7e
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-18 18:43:05 -0700

    Time: 33 ms (57.82%), Space: 56.7 MB (78.96%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -6,7 +6,7 @@ class Solution {
         while (r < nums.length) {
             sum += nums[r];
             // System.out.println(r+" "+l + " " + sum);
-            while (sum + k < (long)nums[r] * (r - l + 1)) {
+            while (l < nums.length && (long)(nums[r] * (r-l+1)) > (long)(sum + k)) {
                 sum -= nums[l];
                 l++;
             }
@@ -18,17 +18,3 @@ class Solution {
 }
 
 
-    // public int maxFrequency(int[] A, int k) {
-    //     int res = 1, i = 0, j;
-    //     long sum = 0;
-    //     Arrays.sort(A);
-    //     for (j = 0; j < A.length; ++j) {
-    //         sum += A[j];
-    //         while (sum + k < (long)A[j] * (j - i + 1)) {
-    //             sum -= A[i];
-    //             i += 1;
-    //         }
-    //         res = Math.max(res, j - i + 1);
-    //     }
-    //     return res;
-    // }
