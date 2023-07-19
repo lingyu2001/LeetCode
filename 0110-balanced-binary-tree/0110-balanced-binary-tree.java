57389628d07ec3145d9c2870279fcff2b69932a9
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-18 19:22:14 -0700

    Time: 1 ms (93.02%), Space: 43.6 MB (13.73%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,15 +14,20 @@
  * }
  */
 class Solution {
+    boolean flag = true;
     public boolean isBalanced(TreeNode root) {
-        return height(root) >= 0;
+        depth(root);
+        return flag;
     }
-    public int height(TreeNode root) {
+    
+    public int depth(TreeNode root) {
         if (root == null) return 0;
-        int left = height(root.left);
-        int right = height(root.right);
-        if (left == -1 || right == -1 || Math.abs(left - right) > 1)
-            return -1;
-        return Math.max(left, right) + 1;
+        int left = depth(root.left);
+        int right = depth(root.right);
+        int rootD = Math.max(left, right) + 1;
+        if (Math.abs(left - right) > 1) {
+            flag = false;
+        }
+        return rootD;
     }
 }
