35eae12d0eb09901ece367b2b2edea9f2dfb107b
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-18 23:08:35 -0700

    Time: 9 ms (5.21%), Space: 41.2 MB (6.81%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] strs = s.split(" ");
+        Map<Character, String> map = new HashMap<>();
+        System.out.println(strs.length + " " + pattern.length());
+        if (strs.length != pattern.length()) return false;
+        for (int i = 0; i < pattern.length(); i++) {
+            if (!map.containsKey(pattern.charAt(i))) {
+                System.out.println(map.values() + " " +strs[i] );
+                if (map.values().contains(strs[i])) return false;
+                map.put(pattern.charAt(i), strs[i]);
+            } else {
+                if (!map.get(pattern.charAt(i)).equals(strs[i])) {
+                    System.out.println(map.get(pattern.charAt(i)) + " " +strs[i] );
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
