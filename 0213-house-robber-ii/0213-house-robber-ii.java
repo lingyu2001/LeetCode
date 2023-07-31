27e1b7cd9862c4a7d31b9f92fb014bae9d70e7b3
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-07-31 12:47:12 -0700

    Time: 0 ms (100.00%), Space: 39.6 MB (97.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -15,12 +15,9 @@ class Solution {
         if (n == 1) return nums[0];
         if (n == 2) return Math.max(nums[0],nums[1]);
         dp[0] = nums[0];
-        dp[1] = nums[1];
+        dp[1] = Math.max(nums[0],nums[1]);
         for (int i = 2; i < n; i++) {
-            dp[i] = dp[i - 1];
-            for (int j = 0; j < i - 1; j++) {
-                dp[i] = Math.max(dp[i], dp[j] + nums[i]);
-            }
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
         return dp[n - 1];
     }
