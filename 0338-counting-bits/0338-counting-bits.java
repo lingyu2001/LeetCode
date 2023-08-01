c05368e2a8406c6a8a0be208c2084fe7e836ef6a
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-01 14:14:46 -0700

    Time: 2 ms (60.90%), Space: 46.2 MB (94.94%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] countBits(int n) {
         int[] dp = new int[n + 1];
-        dp[0] = 0;
-        int index = 1;
-        for (int i = 1; i < dp.length; i++) {
-            if (i == index) {
-                index *= 2;
-                dp[i] = 1 + dp[0];
-            } else if (i < index) {
-                dp[i] = 1 + dp[i - index / 2];
-            }
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = count(i);
         }
         return dp;
+    }
+    
+    public int count(int i) {
+        int cnt = 0;
+        while (i != 0) {
+            i &= (i - 1);
+            cnt++;
+        }
+        return cnt;
     }
 }
