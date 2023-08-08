80c3ea40c4a1772a20410b7fbfc60d844de142c1
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-08 12:41:50 -0700

    Time: 20 ms (36.60%), Space: 57.6 MB (20.39%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (!set.add(nums[i])) {
-                return true;
-            }
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) return true;
         }
         return false;
     }
