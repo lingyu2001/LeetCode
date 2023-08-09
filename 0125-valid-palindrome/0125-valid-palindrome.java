65c711a17a4b882e05c96591c22281a13082d4b3
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-08 21:43:08 -0700

    Time: 3 ms (93.35%), Space: 41.7 MB (96.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
         s = s.toLowerCase();
-        while (i < j) {
-            // make sure pointer i points to the correct character
-            while (i < j && !isAlphanumeric(s.charAt(i))) i++;
-            // make sure pointer j points to the correct character
-            while (i < j && !isAlphanumeric(s.charAt(j))) j--;
-            if (i < j) {
-                if (s.charAt(i) != s.charAt(j)) return false;
+        // System.out.println(s);
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            while (left < s.length() && !isAlphanum(s.charAt(left))) left++;
+            // System.out.print(left + " ");
+            while (right>= 0 && !isAlphanum(s.charAt(right))) right--;
+             // System.out.print(right + " ");
+            // System.out.println();
+            if (left < right && s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else if (left >= right) {
+                return true;
+            } else {
+                return false;
             }
-            i++;
-            j--;
         }
         return true;
     }
-    
-    public boolean isAlphanumeric(char c) {
-        // a-z A-Z,0-9
-        return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    public boolean isAlphanum(char c) {
+        return (c>='a' && c<='z') || (c>='0' && c<= '9');
     }
 }
