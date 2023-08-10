686f00fd572a4df76a092783e6cccfba228e5002
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-09 20:23:11 -0700

    Time: 3 ms (9.91%), Space: 50.9 MB (96.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,13 +4,15 @@ class Solution {
         int[] pre = new int[len];
         int[] post = new int[len];
         pre[0] = 1;
+        post[len - 1] = 1;
         for (int i = 1; i < len; i++) {
             pre[i] = pre[i - 1] * nums[i - 1];
+            post[len - i - 1] = post[len - i] * nums[len - i];
         }
-        post[len - 1] = 1;
-        for (int i = len - 2; i >= 0; i--) {
-            post[i] = post[i + 1] * nums[i + 1];
-        }
+        // post[len - 1] = 1;
+        // for (int i = len - 2; i >= 0; i--) {
+        //     post[i] = post[i + 1] * nums[i + 1];
+        // }
         for (int i = 0; i < len; i++) {
             post[i] *= pre[i];
         }
