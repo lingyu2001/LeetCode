df05f946783250d57f357af1e3cd5340f60f9545
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-10 08:30:53 -0700

    Time: 2 ms (90.06%), Space: 60.5 MB (90.40%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,9 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int left = 0, right = 1;
-        int max = 0;
+        int left = 0, right = 1, max = 0;
         while (right < prices.length) {
-            if (prices[right] - prices[left] > 0) {
+            if (prices[left] < prices[right]) {
                 max = Math.max(max, prices[right] - prices[left]);
                 right++;
             } else {
