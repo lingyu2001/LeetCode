dcd647ed6d2c78c522d06b179c78b37864cb1704
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-09 20:19:39 -0700

    Time: 2 ms (56.83%), Space: 51.6 MB (90.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,42 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] res = new int[nums.length];
-        int[] prefix = new int[nums.length];
-        int[] postfix = new int[nums.length];
-        prefix[0] = nums[0];
-        postfix[nums.length - 1] = nums[nums.length - 1];
-        for (int i = 1; i < nums.length; i++) {
-            if (i - 1 >= 0) {
-                prefix[i] = prefix[i - 1] * nums[i]; 
-            }
+        int len = nums.length;
+        int[] pre = new int[len];
+        int[] post = new int[len];
+        pre[0] = 1;
+        for (int i = 1; i < len; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
         }
-        
-        // for(int i:prefix) {
-        //     System.out.print(i + " ");
-        // }
-        // System.out.println();
-        
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i + 1 < nums.length) {
-                postfix[i] = nums[i] * postfix[i + 1];
-            }
+        post[len - 1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            post[i] = post[i + 1] * nums[i + 1];
         }
-        // for(int i:postfix) {
-        //     System.out.print(i + " ");
-        // }
-        // System.out.println();
-        
-        for (int i = 0; i < nums.length; i++) {
-            res[i] = 1;
+        for (int i = 0; i < len; i++) {
+            post[i] *= pre[i];
         }
+        return post;
         
-        for (int i = 0; i < res.length; i++) {
-            if (i - 1 >= 0) 
-                res[i] *= prefix[i - 1];
-            if (i + 1 < nums.length) {
-                res[i] *= postfix[i + 1];
-            }
-        }
-        return res;
     }
 }
