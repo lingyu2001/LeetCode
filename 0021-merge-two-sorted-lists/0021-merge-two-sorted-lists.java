d614a2edc91c0def725ee67c200397885345a1b8
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-10 23:08:54 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (6.89%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,36 +10,32 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list2 == null && list1 == null)
-            return null;
-        if (list1 != null && list2 == null )
-            return list1;
-        if (list2 != null && list1 == null)
-            return list2;
-        ListNode p1 = list1;
-        ListNode p2 = list2;
-        ListNode head,tail;
-        head = p1.val < p2.val ? p1 : p2;
-        tail = head;
-        if (head == p1)
-            p1 = p1.next;
-        else
-            p2 = p2.next;
-        while(p1 != null && p2 != null) {
+        if (list1 == null && list2 == null) return null;
+        else if (list1 == null) return list2;
+        else  if (list2 == null) return list1;
+        ListNode p1 = list1, p2 = list2;
+        ListNode dummy = new ListNode(), p = dummy;
+        while (p1 != null && p2 != null) {
             if (p1.val < p2.val) {
-                tail.next = p1;
-                tail = tail.next;
+                p.next = new ListNode(p1.val);
+                p = p.next;
                 p1 = p1.next;
             } else {
-                tail.next = p2;
-                tail = tail.next;
+                p.next = new ListNode(p2.val);
+                p = p.next;
                 p2 = p2.next;
             }
         }
-        if (p1 == null) {
-            tail.next = p2;
-        } else
-            tail.next = p1;
-        return head;
-    }
+        while (p1 != null) {
+            p.next = new ListNode(p1.val);
+            p = p.next;
+            p1 = p1.next;
+        }
+        while (p2 != null) {
+            p.next = new ListNode(p2.val);
+            p = p.next;
+            p2 = p2.next;
+        }
+        return dummy.next;
+    } 
 }
