f187bdd73a356e9a64eff0f124ae91fbc920c9b1
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-10 22:58:03 -0700

    Time: 0 ms (100.00%), Space: 39.8 MB (72.67%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return nums[0];
-        if (n == 2) return Math.max(nums[0],nums[1]);
-        int[] nums1 = new int[nums.length - 1];
-        System.arraycopy(nums,0,nums1,0, nums1.length);
-        int[] nums2 = new int[nums.length - 1];
-        System.arraycopy(nums,1,nums2,0, nums2.length);
-        return Math.max(rob1(nums1), rob1(nums2));
-    }
-    public int rob1(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        if (n == 1) return nums[0];
-        if (n == 2) return Math.max(nums[0],nums[1]);
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        if (nums.length == 1) return nums[0];
+        int[] dp1 = new int[nums.length];
+        dp1[0] = 0;
+        dp1[1] = nums[0];
+        for (int i = 2; i < dp1.length; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + nums[i - 1]);
         }
-        return dp[n - 1];
+        int[] dp2 = new int[nums.length];
+        dp2[0] = 0;
+        dp2[1] = nums[1];
+        for (int i = 2; i < dp2.length; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + nums[i]);
+        }
+        return Math.max(dp1[nums.length - 1], dp2[nums.length - 1]);
     }
 }
