b500cb1170ec662ea7e8c07c2b75303d55458744
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-10 22:19:35 -0700

    Time: 0 ms (100.00%), Space: 39.5 MB (96.91%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,12 +15,9 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // if p / q == null return false
         if (p == null && q == null) return true;
-        if (p == null && q != null) return false;
-        if (p != null && q == null) return false;
-        // none of p,q is null
+        else if (p == null || q == null) return false;
         if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return isSameTree(p.left,q.left) && isSameTree(p.right, q.right) ;
     }
 }
