638b19bb6956415baac743705ec09bca46bf9f01
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-11 09:24:47 -0700

    Time: 29 ms (95.65%), Space: 51.8 MB (22.69%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,30 +2,25 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        if (nums[0] > 0 || nums[nums.length - 1] < 0) return res;
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {  // 去重a
-                continue;
-            }
+            // make a distinct
+            while (i - 1 >= 0 && i < nums.length - 2 && nums[i - 1] == nums[i]) i++;
             int target = -nums[i];
             int left = i + 1, right = nums.length - 1;
             while (left < right) {
-                if (nums[left] + nums[right] == target) {
+                if (target == nums[left] + nums[right]) {
                     ArrayList<Integer> arr = new ArrayList<>();
                     arr.add(nums[i]);
                     arr.add(nums[left]);
                     arr.add(nums[right]);
                     res.add(arr);
-                    // 对b，c去重
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
-                } else if (nums[left] + nums[right] < target) {
-                    left++;
-                } else {
+                } else if (target < nums[left] + nums[right]) {
                     right--;
-                }
+                } else left++;
             }
         }
         return res;
