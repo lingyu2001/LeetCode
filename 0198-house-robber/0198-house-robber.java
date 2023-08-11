04961cd8bf917bda4ce19629e99b5017e1a08e83
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-10 22:51:45 -0700

    Time: 0 ms (100.00%), Space: 39.7 MB (89.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,11 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        if (n == 1) return nums[0];
-        if (n == 2) return Math.max(nums[0],nums[1]);
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        for (int i = 2; i < n; i++) {
-            dp[i] = dp[i - 1];
-            for (int j = 0; j < i - 1; j++) {
-                dp[i] = Math.max(dp[i], dp[j] + nums[i]);
-            }
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i < nums.length + 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
-        return dp[n - 1];
+        return dp[nums.length];
     }
 }
