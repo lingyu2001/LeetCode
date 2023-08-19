05090b9b87a15b587d4d05066da585473d75afaa
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-18 18:47:21 -0700

    Time: 149 ms (77.29%), Space: 40.7 MB (55.06%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,13 +1,14 @@
 class Solution {
-    boolean[][] used ;
-    int ROW, COL;
+    int ROW;
+    int COL;
+    boolean[][] used;
     public boolean exist(char[][] board, String word) {
         ROW = board.length;
         COL = board[0].length;
         used = new boolean[ROW][COL];
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
-                if(helper(board,word,i,j,0)) return true;
+                if (helper(board, word, i,j,0)) return true;
             }
         }
         return false;
@@ -15,20 +16,19 @@ class Solution {
     
     public boolean helper(char[][] b, String w, int i, int j, int cIndex) {
         if (cIndex == w.length()) return true;
-        if (i < 0 || 
-            i >= ROW || 
-            j < 0 || 
-            j >= COL||
-            b[i][j] != w.charAt(cIndex) ||
-            used[i][j]) {
-            return false;
-        }
+        if (i < 0||
+           i >= ROW||
+           j < 0||
+           j >= COL||
+           b[i][j] != w.charAt(cIndex)||
+           used[i][j]) return false;
         used[i][j] = true;
-        boolean res = (helper(b,w, i + 1,j,cIndex + 1) ||
-                      helper(b,w, i,j + 1,cIndex + 1) ||
-                      helper(b,w, i - 1,j,cIndex + 1) ||
-                      helper(b,w, i,j - 1,cIndex + 1));
+        boolean res = (
+                        helper(b,w,i+1,j,cIndex+1) ||
+                        helper(b,w,i-1,j,cIndex+1) ||
+                        helper(b,w,i,j+1,cIndex+1) ||
+                        helper(b,w,i,j-1,cIndex+1));
         used[i][j] = false;
         return res;
-    } 
+    }
 }
