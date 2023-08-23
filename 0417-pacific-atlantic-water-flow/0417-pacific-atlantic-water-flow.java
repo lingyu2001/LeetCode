51d4d0a2e36b9c497a075d30e2ff17c98b968813
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-22 22:47:41 -0700

    Time: 4 ms (98.34%), Space: 45.2 MB (10.79%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,40 +1,36 @@
 class Solution {
-
+    List<List<Integer>> res = new ArrayList<>();
     int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-    int rows, cols;
+    int row, col;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> res = new ArrayList<>();
-
-        rows = heights.length;
-        cols = heights[0].length;
-        boolean[][] pacific = new boolean[rows][cols];
-        boolean[][] atlantic = new boolean[rows][cols];
-
-        for (int i = 0; i < cols; i++) {
-            dfs (heights, 0, i, Integer.MIN_VALUE, pacific);
-            dfs (heights, rows - 1, i, Integer.MIN_VALUE, atlantic);
+        row = heights.length;
+        col = heights[0].length;
+        boolean[][] pacific = new boolean[row][col];
+        boolean[][] atlantic = new boolean[row][col];
+        for (int i = 0; i < col; i++) {
+            dfs(heights, 0, i, Integer.MIN_VALUE, pacific);
+            dfs(heights, row - 1, i, Integer.MIN_VALUE, atlantic);
+        }
+        for (int i = 0; i < row; i++) {
+            dfs(heights, i, 0, Integer.MIN_VALUE, pacific);
+            dfs(heights, i, col - 1, Integer.MIN_VALUE, atlantic);
         }
         
-        for (int i = 0; i < rows; i++) {
-            dfs (heights, i, 0, Integer.MIN_VALUE, pacific);
-            dfs (heights, i, cols - 1, Integer.MIN_VALUE, atlantic);
-        }
-        
-        // find the overlappings
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (pacific[i][j] && atlantic[i][j]) res.add(Arrays.asList(i,j));
+        for (int i = 0 ; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (pacific[i][j] && atlantic[i][j])                         
+                    res.add(Arrays.asList(i,j));
             }
         }
         return res;
     }
     
     public void dfs(int[][] heights, int i, int j, int prev, boolean[][] ocean) {
-        if (i < 0 || i >= rows || j < 0 || j >= cols ||
-           ocean[i][j] || heights[i][j] < prev) return;
+        if (i < 0 || j >= col || i >= row || j < 0) return;
+        if (heights[i][j] < prev || ocean[i][j]) return;
         ocean[i][j] = true;
         for (int[] d : dir) {
-            dfs(heights, i + d[0], j + d[1], heights[i][j], ocean);
+            dfs (heights, i + d[0], j + d[1], heights[i][j], ocean);
         }
     }
 }
