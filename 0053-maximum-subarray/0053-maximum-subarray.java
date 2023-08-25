f692e3f8121da92a79f672fa8cde345171d93a67
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-25 10:02:06 -0700

    Time: 1 ms (100.00%), Space: 59.7 MB (47.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE, sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (sum > max) {
-                max = sum;
-            } 
+        // if (nums.length == 1) return nums[0];
+        int start = 0, max = Integer.MIN_VALUE, sum = 0;
+        for (; start < nums.length; start++) {
+            sum += nums[start];
+            max = Math.max(max, sum);
+            // if all the numbers in nums are negative
             if (sum < 0) sum = 0;
         }
         return max;
