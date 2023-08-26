83b46d9ace2d519ce77ffa144d84cbf5a380498f
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-26 10:24:54 -0700

    Time: 52 ms (86.64%), Space: 97.1 MB (66.10%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,20 +1,18 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(a->a[0]));
-        int s = intervals[0][0];
-        int t = intervals[0][1];
         int cnt = 0;
+        int end = intervals[0][1], start = intervals[0][0];
         for (int i = 1; i < intervals.length; i++) {
-            //no overlapping
-            if (intervals[i][0] >= t) {
-                s = intervals[i][0];
-                t = intervals[i][1];
-            } // with overlapping 
-            else {
+            //not overlapping
+            if (end <= intervals[i][0]) {
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }else{
                 cnt++;
-                if (t > intervals[i][1]) {
-                    s = intervals[i][0];
-                    t = intervals[i][1];
+                if (end > intervals[i][1]) {
+                    start = intervals[i][0];
+                    end = intervals[i][1];
                 }
             }
         }
