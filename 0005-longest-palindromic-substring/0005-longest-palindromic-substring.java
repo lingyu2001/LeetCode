22208f0a1967e66e2096d8d069fa49c74a9422ba
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-28 08:10:38 -0700

    Time: 130 ms (33.91%), Space: 56 MB (5.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,26 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
-        boolean[][] dp = new boolean[s.length()][s.length()];
-        int max = 0;
-        String ss = "";
-        for(int i = s.length() - 1; i >= 0; i--) {
-            for(int j = i; j < s.length(); j++) {
+        int[][] dp = new int[s.length()][s.length()];
+        int max = 1;
+        String res = s.substring(0,1);
+        for (int i = 0; i < s.length();i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = s.length() - 2; i >= 0; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
                 if (s.charAt(j) == s.charAt(i)) {
-                   if (j - i <= 1) { // 情况一 和 情况二
-                        dp[i][j] = true;
-                   } else if (dp[i+1][j-1]) {
-                      dp[i][j] = true;
-                   }
-                }
-                if (dp[i][j]) {
-                    if (j - i + 1 > max) {
+                    if (j - i == 1) dp[i][j] = 2;
+                    else if (dp[i+1][j-1] != 0)
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    if (j - i + 1 > max && dp[i][j] != 0) {
                         max = j - i + 1;
-                        ss = s.substring(i, j + 1);
+                        res = s.substring(i, j + 1);
                     }
                 }
             }
         }
-        if (ss.equals(" ")) ss = s.substring(0,1);
-        return ss;
+        return res;
     }
 }
