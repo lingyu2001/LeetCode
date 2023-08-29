a44f200e830c3556fafd6903a632223435d778f2
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-29 08:46:21 -0700

    Time: 0 ms (100.00%), Space: 43.9 MB (75.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int ans = 0;
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            ans = ans ^ nums[i] ^ i;
+        int ans = nums[0];
+        for (int i = 1; i < n; i++) {
+            ans = ans ^ nums[i];
         }
-        ans ^= n;
+        for (int i = 1; i <= n; i++) {
+            ans = ans ^ i;
+        }
         return ans;
     }
 }
