0cbae5db1de28289a0e050996cc4f8c6adc9bb65
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-31 08:03:41 -0700

    Time: 18 ms (95.01%), Space: 46.3 MB (97.60%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->-Integer.compare(a[0]*a[0] + a[1]*a[1],
-                b[0]*b[0] + b[1]*b[1])
-        );
-        for (int i = 0; i < points.length; i++) {
-            pq.add(points[i]);
-            if (pq.size() > k) 
-                pq.remove();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0]+a[1]*a[1]));
+        for (int[] p : points) {
+            if (pq.size() < k) pq.offer(p);
+            else {
+                int len = p[0] * p[0] + p[1] * p[1];
+                if (len < length(pq.peek())) {
+                    pq.poll();
+                    pq.offer(p);
+                }
+            }
         }
-        
-        int[][] ans = new int[k][2];
-        for(int i = 0; i < k; i++) {
-            int[]t = pq.poll();
-            ans[i][0] = t[0];
-            ans[i][1] = t[1];
+        int[][] res = new int[k][2];
+        int index = 0;
+        for (int[] p : pq) {
+            res[index++] = p;
         }
-        return ans;
+        return res;
     }
     
-    public int dis(int[] p) {
-        return p[0] * p[0] + p[1] * p[1];
+    public int length(int[] p) {
+        return p[0]*p[0]+p[1]*p[1];
     }
 }
