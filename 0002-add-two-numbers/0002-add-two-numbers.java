71d81b8c026908fd640a862fd4b998f41a397c3e
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-01 12:47:39 -0700

    Time: 1 ms (100.00%), Space: 43.2 MB (65.48%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,28 +10,23 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode p1 = l1, p2 = l2;
         int digit = 0;
-        int res = 0;
         ListNode head = new ListNode();
-        ListNode t = head;
-        ListNode p = l1, q = l2;
-        while (p != null || q != null) {
-            res *= 10;
-            int pval = p == null? 0 : p.val;
-            int qval = q == null? 0 : q.val;
-            t.val = (pval + qval + digit) % 10;
-            digit = (pval + qval + digit) / 10;
-            p = p == null ? null : p.next;
-            q = q == null ? null : q.next;
-            if (p != null || q != null) {
-                t.next = new ListNode();
-                t = t.next;
+        ListNode p = head;
+        while (p1 != null || p2 != null) {
+            int p1val = p1 == null ? 0 : p1.val;
+            int p2val = p2 == null ? 0 : p2.val;
+            p.val = (p1val + p2val + digit) % 10;
+            digit = (p1val + p2val + digit) / 10;
+            p1 = p1 == null ? null : p1.next;
+            p2 = p2 == null ? null : p2.next;
+            if (p1 != null || p2 != null) {
+                p.next = new ListNode();
+                p = p.next;
             }
         }
-        if (digit > 0) {
-            t.next = new ListNode(digit);
-        }
-        
+        if (digit != 0) p.next = new ListNode(digit);
         return head;
     }
 }
