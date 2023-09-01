8f63f3918566bc17ab8ffd036498c0f40fe1f63f
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-08-31 22:03:04 -0700

    Time: 45 ms (83.89%), Space: 128.2 MB (10.01%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,66 +1,66 @@
 class LRUCache {
-
+    
     private class Node {
-        int key, value;
-        Node prev, next;
-        public Node(int key, int value) {
+        int key, val;
+        Node prev,next;
+        public Node(int key, int val) {
             this.key = key;
-            this.value = value;
+            this.val = val;
         }
     }
-
-    private final int capacity;
-    private final Map<Integer, Node> map;
-    private final Node head, tail;
-
+    Node head,tail;
+    int size;
+    HashMap<Integer, Node> map;
     public LRUCache(int capacity) {
-        this.capacity = capacity;
-        map = new HashMap<>();
+        size = capacity;
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
         head.next = tail;
         tail.prev = head;
+        map = new HashMap<>();
     }
-
+    
     public int get(int key) {
         if (!map.containsKey(key)) return -1;
-
-        // Move the accessed node to the head
-        Node node = map.get(key);
-        removeFromList(node);
-        addToList(node);
-
-        return node.value;
+        else {
+            Node n = map.get(key);
+            removeFromList(n);
+            addToList(n);
+            return n.val;
+        }
     }
-
+    
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            // Remove the old value
             removeFromList(map.get(key));
             map.remove(key);
-        }
-
-        // Check if we're over capacity
-        if (map.size() == capacity) {
+        } 
+        if (map.size() >= size) {
             map.remove(tail.prev.key);
             removeFromList(tail.prev);
         }
-
-        // Add the new node to the head of the list and to the map
-        Node node = new Node(key, value);
+        Node node = new Node(key,value);
         addToList(node);
         map.put(key, node);
     }
-
-    private void removeFromList(Node node) {
+    
+    public void removeFromList(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
-
-    private void addToList(Node node) {
+    
+    public void addToList(Node node) {
         node.next = head.next;
         node.prev = head;
         head.next.prev = node;
         head.next = node;
+        
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
