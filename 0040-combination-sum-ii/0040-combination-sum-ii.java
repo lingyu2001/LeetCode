1f0a3b89de9ee433959c1ef58b8ea27206795a19
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-01 20:01:14 -0700

    Time: 4 ms (50.98%), Space: 44.2 MB (6.42%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,21 +3,20 @@ class Solution {
     LinkedList<Integer> path = new LinkedList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        helper(candidates, 0, target);
+        helper(candidates, target, 0);
         return res;
     }
     
-    public void helper(int[] c, int k, int target) {
-        if (target < 0) return;
+    public void helper(int[] c, int target, int index) {
+        if (target < 0) return ;
         if (target == 0) {
             res.add(new ArrayList<>(path));
             return;
         }
-        
-        for(int i = k; i < c.length; i++) {
-            if (i > k && c[i] == c[i - 1]) continue;
+        for (int i = index; i < c.length; i++) {
+            if (i > index && c[i] == c[i - 1]) continue;
             path.add(c[i]);
-            helper(c, i + 1, target - c[i]);
+            helper(c, target - c[i], i + 1);
             path.removeLast();
         }
     }
