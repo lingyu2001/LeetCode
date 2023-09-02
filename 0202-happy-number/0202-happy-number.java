40d90b31e09729e2303e35540f0775ff5f8317d0
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-01 21:42:17 -0700

    Time: 1 ms (87.18%), Space: 40.3 MB (6.23%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,20 +1,16 @@
 class Solution {
+    Set<Integer> set = new HashSet<>();
     public boolean isHappy(int n) {
-        int nn = n;
-        Set<Integer> set = new HashSet<>();
         while (true) {
             int sum = 0;
-            while (n > 0) {
-                int digit = n % 10;
-                sum += digit * digit;
+            while (n != 0) {
+                sum += (n % 10) * (n % 10);
                 n = n / 10;
             }
             // System.out.println(sum);
-            if (set.contains(sum))
-                return false;
-            else if (sum == 1)
-                return true;
-            else {
+            if (sum == 1) return true;
+            else if (set.contains(sum)) return false;
+            else { 
                 set.add(sum);
                 n = sum;
             }
