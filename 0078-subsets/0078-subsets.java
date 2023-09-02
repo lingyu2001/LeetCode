77e32424436e474f86e50f7cfc532281bc58f59d
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-01 19:19:04 -0700

    Time: 1 ms (70.98%), Space: 42.4 MB (5.62%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,20 +2,16 @@ class Solution {
     List<List<Integer>> res = new ArrayList<>();
     LinkedList<Integer> path = new LinkedList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        helper(nums,0,nums.length);
+        helper(nums, 0);
         return res;
     }
     
-    public void helper(int[] nums, int i, int j) {
-        res.add(new ArrayList<>(path));
-        if (i >= j) {
-            return ;
-        }
-        for (int k = i; k < j; k++) {
-            path.add(nums[k]);
-            helper(nums,k + 1,nums.length);
-            path.removeLast();
-        }
-        return;
-    }
+     public void helper(int[] nums, int index) {
+         res.add(new ArrayList<>(path));
+         for (int i = index ; i < nums.length; i++) {
+             path.add(nums[i]);
+             helper(nums, i + 1);
+             path.removeLast();
+         }
+     }
 }
