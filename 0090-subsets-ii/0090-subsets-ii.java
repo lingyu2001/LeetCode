4adce12760d0d245b669534041acad5e2e40a298
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-01 19:48:55 -0700

    Time: 1 ms (100.00%), Space: 44.2 MB (7.42%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,20 +1,23 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
     LinkedList<Integer> path = new LinkedList<>();
+    boolean[] used;
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        helper(nums,0);
+        used = new boolean[nums.length];
+        helper(nums, 0);
         return res;
     }
     
-    public void helper(int[] nums, int k) {
+    public void helper(int[] nums, int index) {
         res.add(new ArrayList<>(path));
-        // k <= n
-        for (int i = k; i < nums.length; i++) {
-            if (i > k && nums[i] == nums[i - 1]) continue;
+        for (int i = index; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
             path.add(nums[i]);
             helper(nums, i + 1);
             path.removeLast();
+            used[i] = false;
         }
     }
 }
