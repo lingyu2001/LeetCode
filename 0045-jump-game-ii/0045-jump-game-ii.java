3b1e5c259b4b23b1d90aa05cb13fda14ce53ea47
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-04 11:01:55 -0700

    Time: 42 ms (23.17%), Space: 44 MB (95.94%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        if (nums.length == 1) return 0;
-        int cnt = 0;
-        int currDis = 0, nextDis = 0;
-        for (int i = 0; i < nums.length; i++) {
-            nextDis = Math.max(nextDis, i + nums[i]);
-            if (i == currDis) {
-                cnt++;
-                currDis = nextDis;
-                if (nextDis >= nums.length - 1) break;
-            }
-            
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = Integer.MAX_VALUE - 1;
         }
-        return cnt;
+        dp[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 1; j <= nums[i] && i + j < n; j++) {
+                dp[i] = Math.min(dp[i], dp[i + j] + 1);
+            }
+        }
+        return dp[0];
     }
 }
