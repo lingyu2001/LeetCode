5c4443373c63ed09103c92a8dd4152c9e279d570
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-07 08:00:19 -0700

    Time: 1 ms (91.71%), Space: 42.3 MB (76.94%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,25 +2,24 @@ class Solution {
     List<String> res = new ArrayList<>();
     StringBuilder sb = new StringBuilder();
     public List<String> generateParenthesis(int n) {
-        helper(n, 0,0);
+        helper(0,0,n);
         return res;
     }
     
-    public void helper(int n, int m1, int m2) {
-        if (m1 == n && m2 == n) {
-            res.add(new String(sb.toString()));
-            return;
+    public void helper(int m1, int m2, int n) {
+        if(m1 == n && m2 == n) {
+            res.add(sb.toString());
         } else if (m1 < m2) {
             return ;
-        } else if (m1 <= n){
+        } else if (m1 <= n) {
             sb.append("(");
-            helper(n, m1 + 1, m2);
+            helper(m1 + 1, m2, n);
             sb.deleteCharAt(sb.length() - 1);
             sb.append(")");
-            helper(n, m1, m2 + 1);
+            helper(m1, m2 + 1, n);
             sb.deleteCharAt(sb.length() - 1);
         } else {
-            return;
+            return ;
         }
     }
 }
