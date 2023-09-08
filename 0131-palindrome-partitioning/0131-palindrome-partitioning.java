0d891227dea061e8e547a5bac4a5530943a6fb0d
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-08 09:44:25 -0700

    Time: 9 ms (46.16%), Space: 55.1 MB (80.30%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,26 +2,27 @@ class Solution {
     List<List<String>> res = new ArrayList<>();
     LinkedList<String> path = new LinkedList<>();
     public List<List<String>> partition(String s) {
-        helper(s, 0, s.length() - 1);
+        helper(s, 0);
         return res;
     }
     
-    public void helper(String s, int start, int end) {
-        if (start > end) {
-            res.add(new ArrayList<>(path)) ;
+    public void helper(String s, int start) {
+        if (start >= s.length()) {
+            res.add(new ArrayList<>(path));
             return ;
         }
-        for (int i = start; i <= end; i++) {
-            if (!isPalindrome(s, start, i)) continue;
-            path.add(s.substring(start, i + 1));
-            helper(s, i + 1, end);
-            path.removeLast();
+        for (int i = start; i < s.length(); i++) {
+            if (isValid(s, start, i)) {
+                path.add(s.substring(start, i + 1));
+                helper(s, i + 1);
+                path.removeLast();
+            }
         }
     }
     
-    public boolean isPalindrome(String s, int left, int right) {
+    public boolean isValid(String s, int left, int right) {
         if (right < left) return false;
-        while (left <= right) {
+        while (left < right) {
             if (s.charAt(left) != s.charAt(right)) return false;
             left++;
             right--;
