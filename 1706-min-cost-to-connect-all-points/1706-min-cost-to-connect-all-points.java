d2fa2109c9b6d61c14b63754c91a4d9571702ddc
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-11 13:31:11 -0700

    Time: 102 ms (71.38%), Space: 60.4 MB (56.66%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,27 +1,22 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        // add edges and distance to priority queue
-        // poll an edge and connect the points if there isn't a cycle
-        // if there is a cycle, continue
-        int n = points.length;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
-        Set<Integer> set = new HashSet<>();
-        pq.offer(new int[]{0, 0});
+        pq.offer(new int[]{0 ,0});
         int cost = 0;
+        Set<Integer> set = new HashSet<>();
+        int n = points.length;
         while (set.size() < n) {
-            int[] t = pq.poll();
-            int weight = t[0];
-            int currNode = t[1];
-            if (set.contains(currNode)) continue;
-            set.add(currNode);
-            cost += weight;
+            int[] cur = pq.poll();
+            int curNode = cur[1];
+            int curWeight = cur[0];
+            if (set.contains(curNode)) continue;
+            cost += curWeight;
+            set.add(curNode);
             for (int i = 0; i < n; i++) {
                 if (set.contains(i)) continue;
-                pq.offer(new int[] {
-                    Math.abs(points[currNode][0] - points[i][0]) + 
-                    Math.abs(points[currNode][1] - points[i][1]),
-                    i
-                });
+                pq.offer(new int[] { Math.abs(points[curNode][0] - points[i][0]) + 
+                Math.abs(points[curNode][1] - points[i][1])
+                , i});
             }
         }
         return cost;
