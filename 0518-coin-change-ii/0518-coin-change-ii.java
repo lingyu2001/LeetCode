5ba5f95185660a48a031280e58fab5ba048ead0c
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-11 16:41:09 -0700

    Time: 3 ms (75.96%), Space: 40.2 MB (68.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,11 +1,10 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        Arrays.sort(coins);
         int[] dp = new int[amount + 1];
         dp[0] = 1;
         for (int c : coins) {
-            for (int i = 0; i <= amount; i++) {
-                if (i >= c) {
+            for (int i = 1; i <= amount; i++) {
+                if (c <= i) {
                     dp[i] += dp[i - c];
                 }
             }
