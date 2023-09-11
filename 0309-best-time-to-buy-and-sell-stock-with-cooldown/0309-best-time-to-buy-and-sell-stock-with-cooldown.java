99a4919e8ea6c0b194c528fdf6d14954defe999c
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-11 16:37:20 -0700

    Time: 1 ms (86.96%), Space: 40.7 MB (43.4%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,7 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int [prices.length][2];
-        for (int i = 0; i < prices.length; i++) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
             if (i == 0) {
                 dp[i][0] = 0;
                 dp[i][1] = -prices[i];
@@ -9,12 +10,12 @@ class Solution {
             }
             if (i == 1) {
                 dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], -prices[i]); 
                 continue;
             }
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
         }
-        return dp[prices.length - 1][0];
+        return dp[n - 1][0];
     }
 }
