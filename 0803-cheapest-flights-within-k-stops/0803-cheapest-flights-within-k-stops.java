c663f4ed6a540e8f288964ba8e1963a77bd3b8c6
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-11 16:23:21 -0700

    Time: 5 ms (96.6%), Space: 43.8 MB (34.11%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,22 +1,20 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] dist = new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[src] = 0;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[src] = 0;
         for (int i = 0; i <= k; i++) {
             int[] temp = new int[n];
-            System.arraycopy(dist, 0, temp, 0, n);
+            System.arraycopy(dp, 0, temp, 0, n);
             for (int j = 0; j < flights.length; j++) {
-                int s = flights[j][0];
-                int d = flights[j][1];
-                int p = flights[j][2];
-                if (dist[s] == Integer.MAX_VALUE) continue;
-                if (dist[s] + p < temp[d]) {
-                    temp[d] = dist[s] + p; 
+                if (dp[flights[j][0]] != Integer.MAX_VALUE) {
+                    if (dp[flights[j][0]] + flights[j][2] < temp[flights[j][1]]) {
+                        temp[flights[j][1]] = dp[flights[j][0]] + flights[j][2];
+                    }
                 }
             }
-            dist = temp;
+            dp = temp;
         }
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return dp[dst] == Integer.MAX_VALUE ? -1 : dp[dst];
     }
 }
