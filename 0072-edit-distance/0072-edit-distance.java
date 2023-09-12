5d93a45b11a161e3107b93fe0a9a0e669a76b68f
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-11 20:52:06 -0700

    Time: 3 ms (96.97%), Space: 43.7 MB (51.91%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,22 +1,43 @@
+//Refer to neetcode's video
+
+//Memoized code
+
 class Solution {
+
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
-        //init
-        for (int i = 0; i < word1.length() + 1; i++) {
-            dp[i][0] = i;
+        int m = word1.length() - 1;
+        int n = word2.length() - 1;
+        int[][] dp = new int[m + 2][n + 2];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
         }
-        for (int j = 0; j < word2.length() + 1; j++) {
-            dp[0][j] = j;
+        return helper(word1, word2, m, n, dp);
+    }
+
+    public int helper(String word1, String word2, int m, int n, int[][] dp) {
+        //the strings are null
+        if (m + 1 == 0 && n + 1 == 0) {
+            return 0;
         }
-        for (int i = 1; i <= word1.length(); i++) {
-            for (int j = 1; j <= word2.length(); j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.min(dp[i - 1][j] + 1, Math.min(dp[i - 1][j - 1] + 1, dp[i][j - 1] + 1));
-                }
-            }
+        //one of the strings are null
+        if (m + 1 == 0 || n + 1 == 0) {
+            return Math.max(m + 1, n + 1);
         }
-        return dp[word1.length()][word2.length()];
+        //both values at the index are equal
+        if (dp[m][n] != -1) return dp[m][n];
+        if (word1.charAt(m) == word2.charAt(n)) {
+            dp[m][n] = helper(word1, word2, m - 1, n - 1, dp);
+            return dp[m][n];
+        } else {
+            //try deletion
+            int delete = 1 + helper(word1, word2, m - 1, n, dp);
+            //try insertion
+            int insert = 1 + helper(word1, word2, m, n - 1, dp);
+            //try replacing
+            int replace = 1 + helper(word1, word2, m - 1, n - 1, dp);
+            //now we'll choose the minimum out of these 3 and add 1 for the operation cost
+            dp[m][n] = Math.min(Math.min(delete, insert), replace);
+            return dp[m][n];
+        }
     }
 }
