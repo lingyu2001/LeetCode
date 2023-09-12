5e1525bae2904b40a7ea84d5f0db0b29cca331de
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-11 20:55:25 -0700

    Time: 6 ms (29.77%), Space: 43.3 MB (88.64%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,22 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-    int m = word1.length();
-    int n = word2.length();
-    int[][] dp = new int[m + 1][n + 1];
-    // 初始化
-    for (int i = 1; i <= m; i++) {
-        dp[i][0] =  i;
-    }
-    for (int j = 1; j <= n; j++) {
-        dp[0][j] = j;
-    }
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            // 因为dp数组有效位从1开始
-            // 所以当前遍历到的字符串的位置为i-1 | j-1
-            if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                dp[i][j] = dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        //init
+        for (int i = 0; i < word1.length() + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < word2.length() + 1; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j] + 1, Math.min(dp[i - 1][j - 1] + 1, dp[i][j - 1] + 1));
+                }
             }
         }
+        return dp[word1.length()][word2.length()];
     }
-    return dp[m][n];
-}
 }
