d68df3d5a5cbc39e2471f3cc9d72ac6e17d2f8e5
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-11 21:10:04 -0700

    Time: 0 ms (100%), Space: 41.5 MB (12.48%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,25 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0, j = 0;
-        // find the row index
-        int up = 0, down = matrix.length;
-        while (up < down) {
-            i = up + (down - up) / 2;
-            if (matrix[i][0] == target) return true;
-            if (i - 1 >= 0 && matrix[i][0] > target && matrix[i - 1][0] < target){
-                i = i - 1;
-                break;
-            }else if (i + 1 < matrix.length && matrix[i][0] < target && matrix[i + 1][0] > target){
-                break;
-            }else if (matrix[i][0] > target) down = i;
-            else up = i + 1;
-        }
-        int left = 0, right = matrix[i].length;
-        while (left < right) {
-            j = left + (right - left) / 2;
-            if (matrix[i][j] == target) return true;
-            else if (matrix[i][j] < target) left = j + 1;
-            else right = j;
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+		 return true;
+	    } else if (matrix[i][j] > target) {
+		 j--;
+	    } else {
+		 i++;
+	    }
         }
         return false;
     }
