ca20fd40425c4422f075d01168306f9dd2fef320
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-13 09:44:06 -0700

    Time: 1 ms (98.69%), Space: 40.4 MB (77.1%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,15 @@
 class Solution {
-    Stack<Character> stack = new Stack<>();
     public boolean isValid(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '}') {
-                if (stack.isEmpty() || stack.pop() != '{') 
-                    return false;
-            } else if (s.charAt(i) == ']') {
-                 if (stack.isEmpty() || stack.pop() != '[') 
-                    return false;      
-            } else if (s.charAt(i) == ')'){
-                if (stack.isEmpty() || stack.pop() != '(') 
-                    return false;      
-            } else {
-                stack.push(s.charAt(i));
-            }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') return false;
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') return false;
+            } else if (c == ')'){
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            } else 
+                stack.push(c);
         }
         return stack.isEmpty();
     }
