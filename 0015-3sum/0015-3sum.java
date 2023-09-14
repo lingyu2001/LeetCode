253661db2e031097435baac572a9c4380b19d417
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-13 19:42:57 -0700

    Time: 33 ms (59.85%), Space: 50.9 MB (45.6%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,29 @@
 class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new LinkedList<>();
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+        if (nums[0] > 0 || nums[nums.length - 1] < 0) return res;
         for (int i = 0; i < nums.length - 2; i++) {
-            // make a distinct
-            while (i - 1 >= 0 && i < nums.length - 2 && nums[i - 1] == nums[i]) i++;
+            //  distinct 1
+            if(i >= 1 && nums[i] == nums[i - 1]) continue;
             int target = -nums[i];
-            int left = i + 1, right = nums.length - 1;
-            while (left < right) {
-                if (target == nums[left] + nums[right]) {
-                    ArrayList<Integer> arr = new ArrayList<>();
-                    arr.add(nums[i]);
-                    arr.add(nums[left]);
-                    arr.add(nums[right]);
-                    res.add(arr);
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++;
-                    right--;
-                } else if (target < nums[left] + nums[right]) {
-                    right--;
-                } else left++;
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] == target) {
+                    path.add(nums[i]);
+                    path.add(nums[j]);
+                    path.add(nums[k]);
+                    res.add(new ArrayList<>(path));
+                    path.clear();
+                    j++;
+                    k--;
+                    while (j < nums.length && nums[j] == nums[j - 1]) j++;
+                    while (k >= 0 && nums[k + 1] == nums[k]) k--;
+                }
+                else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else j++;
             }
         }
         return res;
