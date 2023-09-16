93d9c5594936a1d949db5b0e4df674f5784dc265
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-15 22:32:04 -0700

    Time: 0 ms (100%), Space: 40.4 MB (98.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,8 +5,8 @@ class Solution {
             int mid = left + (right - left) / 2;
             if (target == nums[mid]) return mid;
             if (nums[left] <= nums[mid]) {
-                if (target > nums[mid] || target < nums[left]) left = mid + 1;
-                else right = mid - 1;
+                if (target >= nums[left] && target <= nums[mid]) right = mid - 1;
+                else left = mid + 1;
             } else {
                 if (target >= nums[mid] && target <= nums[right]) left = mid + 1;
                 else right = mid - 1;
