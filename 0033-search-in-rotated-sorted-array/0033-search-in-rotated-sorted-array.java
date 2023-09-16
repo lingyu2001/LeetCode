e33ac33a02cbebcecfea10bf36c25f0646a7573b
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-15 22:30:22 -0700

    Time: 0 ms (100%), Space: 41.2 MB (24.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,8 +8,8 @@ class Solution {
                 if (target > nums[mid] || target < nums[left]) left = mid + 1;
                 else right = mid - 1;
             } else {
-                if (target < nums[mid] || target > nums[right]) right = mid - 1;
-                else left = mid + 1;
+                if (target >= nums[mid] && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
             }
         }
         return -1;
