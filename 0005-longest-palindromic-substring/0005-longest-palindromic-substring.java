1d47de8e4e6ceebd06c60595c4a432a3a2cd4b4d
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-16 19:22:34 -0700

    Time: 140 ms (30.63%), Space: 46.6 MB (8.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,22 +1,15 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int[][] dp = new int[s.length()][s.length()];
-        int max = 1;
-        String res = s.substring(0,1);
-        for (int i = 0; i < s.length();i++) {
-            dp[i][i] = 1;
-        }
-        for (int i = s.length() - 2; i >= 0; i--) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (s.charAt(j) == s.charAt(i)) {
-                    if (j - i == 1) dp[i][j] = 2;
-                    else if (dp[i+1][j-1] != 0)
-                        dp[i][j] = dp[i + 1][j - 1] + 2;
-                    if (j - i + 1 > max && dp[i][j] != 0) {
-                        max = j - i + 1;
-                        res = s.substring(i, j + 1);
-                    }
+        int n = s.length();
+        String res = s.substring(0, 1);
+        boolean [][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (i + 1 >= j) dp[i][j] = true;
+                    else dp[i][j] = dp[i + 1][j - 1];
                 }
+                if (dp[i][j] && (j - i + 1) > res.length()) res = s.substring(i, j + 1);
             }
         }
         return res;
