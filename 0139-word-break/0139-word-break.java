3100680e990a8acf283c24d2f04a0a122099ec63
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-16 17:57:17 -0700

    Time: 7 ms (50.19%), Space: 43.3 MB (56.1%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -4,11 +4,13 @@ class Solution {
         dp[s.length()] = true;
         for (int i = s.length() - 1; i >= 0; i--) {
             for (String w : wordDict) {
-                if (i + w.length() <= s.length() && s.startsWith(w, i)) {
-                    dp[i] = dp[i + w.length()];
+                if (i + w.length() <= s.length() && s.substring(i, i + w.length()).equals(w)) {
+                    //  System.out.println(s.substring(i, i + w.length()) + " " + w);
+                     dp[i] = dp[i] || dp[i + w.length()];
                 }
-                if (dp[i]) break;
             }
+            // for (boolean m : dp) System.out.print(m + " ");
+            // System.out.println("==================");
         }
         return dp[0];
     }
