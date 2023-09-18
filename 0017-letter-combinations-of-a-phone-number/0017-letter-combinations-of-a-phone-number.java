f4bc18672a5e5d9c429121739a2ef3ffef9202c9
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-18 11:13:19 -0700

    Time: 1 ms (79%), Space: 40.8 MB (91.96%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,22 +1,22 @@
 class Solution {
     List<String> res = new ArrayList<>();
     StringBuilder sb = new StringBuilder();
-    String[] strs = new String[] {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    String[] strs = new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) return res;
         helper(digits, 0);
         return res;
     }
-    
+
     public void helper(String digits, int index) {
         if (sb.length() == digits.length()) {
             res.add(sb.toString());
             return;
-        } 
+        }
         for (int i = index; i < digits.length(); i++) {
-            int num = digits.charAt(i) - '0';
-            for (int j = 0; j < strs[num].length(); j++) {
-                sb.append(strs[num].charAt(j));
+            String s = strs[digits.charAt(i) - '0'];
+            for (int j = 0; j < s.length(); j++) {
+                sb.append(s.charAt(j));
                 helper(digits, i + 1);
                 sb.deleteCharAt(sb.length() - 1);
             }
