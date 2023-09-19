d9f8490028844bf61d2d9b60a33645be77362f0e
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-18 19:33:53 -0700

    Time: 38 ms (66.35%), Space: 46.6 MB (54.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,14 +5,12 @@ class Solution {
         if (sum % 2 != 0) return false;
         sum = sum / 2;
         boolean[][] dp = new boolean[nums.length + 1][sum + 1];
-        for (int i = 0; i <= nums.length; i++) {
-            dp[i][0] = true;
-        }
-        for (int i = 1; i < nums.length + 1; i++) {
+        for (int i = 0; i < nums.length; i++) dp[i][0] = true;
+        for (int i = 1; i <= nums.length; i++) {
             for (int j = 1; j <= sum; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-                } else dp[i][j] = dp[i - 1][j];
+                if (j < nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
             }
         }
         return dp[nums.length][sum];
