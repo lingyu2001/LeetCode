ad81bf36fb7b47db4a0a934bc518a0cc47ed5c56
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-20 13:20:13 -0700

    Time: 26 ms (42.6%), Space: 44.8 MB (32.05%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,13 +1,12 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int time = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b- a);
-       Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         int[] arr = new int[26];
-        for (char c: tasks) arr[c - 'A']++;
-        for (int i : arr) if (i > 0) pq.offer(i);
-        
-        while (!pq.isEmpty() || !q.isEmpty()) {
+        for(char c : tasks) arr[c - 'A']++;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        for (int i : arr) if (i != 0) pq.offer(i);
+        int time = 0;
+        while (!q.isEmpty() || !pq.isEmpty()) {
             time++;
             if (!pq.isEmpty()) {
                 int val = pq.poll();
@@ -15,7 +14,7 @@ class Solution {
                 if (val > 0) q.offer(new Pair(val, time + n));
             }
             if (!q.isEmpty() && q.peek().getValue() == time) {
-                pq.add(q.poll().getKey());
+                pq.offer(q.poll().getKey());
             }
         }
         return time;
