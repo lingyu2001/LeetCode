7ad5593ab5706bb5e566fc1f023a32eb832a7f38
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-19 22:18:53 -0700

    Time: 0 ms (100%), Space: 42.8 MB (85.43%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,15 +15,14 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return dfs(root) != -1;
+        return helper(root) == -1 ? false : true;
     }
-    
-    public int dfs(TreeNode root) {
+
+    public int helper(TreeNode root) {
         if (root == null) return 0;
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        if (left == -1 || right == -1 || Math.abs(left - right) > 1)
-            return -1;
-        else return Math.max(left, right) + 1;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if (left == -1 || right == -1 || Math.abs(right - left) > 1) return -1;
+        return Math.max(left, right) + 1;
     }
 }
