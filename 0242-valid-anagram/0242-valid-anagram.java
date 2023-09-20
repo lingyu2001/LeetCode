763b9a4acb0879bfb7e410b3760bc3a40dd60c5d
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-19 22:04:09 -0700

    Time: 6 ms (40.83%), Space: 41.7 MB (92.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // check if len(s) == len(t), no--> return false
-        int len1 = s.length();
-        int len2 = t.length();
-        if (len1 != len2) return false;
-        // store s in the set
-        int[] chars = new int[26];
-        for (int i = 0; i < len1; i++) {
-            chars[s.charAt(i) - 'a']++;
+        if (s.length() != t.length()) return false;
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
-        // check t
-        // iterate each char c in t
-        for (int i = 0; i < len2; i++) {
-            chars[t.charAt(i) - 'a']--;
-            if (chars[t.charAt(i) - 'a'] < 0) return false;
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) return false;
         }
         return true;
-        // if the number of c in the set < 0, then return false;
-
     }
 }
