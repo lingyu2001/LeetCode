eac55cb17776387f77b662cceb8d5fb3cc4e5363
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-19 22:37:42 -0700

    Time: 3 ms (78.54%), Space: 43.3 MB (97.4%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,15 +1,17 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        int[] ints = new int[26];
-        for (int i = 0; i < magazine.length(); i++) {
-            ints[magazine.charAt(i) - 'a']++;
+    public boolean canConstruct(String randomNote, String magazine) {
+        if (randomNote.length() > magazine.length()) return false;
+        int[] arr = new int[26];
+        for (int i = 0; i < randomNote.length(); i++) {
+            arr[randomNote.charAt(i) - 'a']--;
+            arr[magazine.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < ransomNote.length(); i++) {
-            if (ints[ransomNote.charAt(i) - 'a'] <= 0)
-                return false;
-            else {
-                ints[ransomNote.charAt(i) - 'a']--;
-            }
+        for (int i = randomNote.length(); i < magazine.length(); i++) {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            // System.out.println(i +","+arr[i]);
+            if (arr[i] < 0) return false;
         }
         return true;
     }
