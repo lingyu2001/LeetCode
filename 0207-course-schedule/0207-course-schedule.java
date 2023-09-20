8933666396dd5b31a861b3e9035719d2e794c563
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-20 12:16:38 -0700

    Time: 5 ms (65.74%), Space: 44.1 MB (57.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,33 @@
 class Solution {
-    int[] used;
-    Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+    int[] visited;
+    Map<Integer, Set<Integer>> map = new HashMap<>();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // create a map
-        for (int i = 0; i < numCourses; i++) {
-            map.put(i, new ArrayList<Integer>());
+        visited = new int[numCourses];
+        for (int[] p : prerequisites) {
+            Set<Integer> set1 = map.getOrDefault(p[0], new HashSet<>());
+            set1.add(p[1]);
+            map.put(p[0],set1);
         }
-        for (int[] pre : prerequisites) {
-            map.get(pre[0]).add(pre[1]);
-        }
-        used = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (isCyclic(prerequisites, i)) return false;
+            boolean flag = false;
+            if (visited[i] == 0) {
+                flag = dfs(i);
+                if (!flag) return false;
+            } else if (visited[i] == 1) continue;
         }
         return true;
     }
-    
-    public boolean isCyclic(int[][] prerequisites, int n) {
-        if (used[n] == 2) {
-            return true;
-        } else if (used[n] == 1) return false;
-        used[n] = 2;
-        ArrayList<Integer> list = map.get(n);
-        for (int i = 0; i < list.size(); i++) {
-                if (isCyclic(prerequisites, list.get(i))) {
-                    return true;
-                }
-        }
-        used[n] = 1;
-        return false;
+
+    public boolean dfs(int i) {
+        if (visited[i] == 1) return true;
+        if (visited[i] == 2) return false;
+        visited[i] = 2;
+        Set<Integer> set = map.get(i);
+        if (set != null)
+            for (int j : set) {
+                if (!dfs(j)) return false;
+            }
+        visited[i] = 1;
+        return true;
     }
 }
