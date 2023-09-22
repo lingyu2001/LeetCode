3f0a7125be8c07179b50c9c213a7c252351ae26a
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-22 15:03:43 -0700

    Time: 6 ms (45.85%), Space: 44.8 MB (12.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,19 +1,15 @@
 class Solution {
+    Map<Integer, HashSet<Integer>> map = new HashMap<>();
     int[] visited;
-    Map<Integer, Set<Integer>> map = new HashMap<>();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         visited = new int[numCourses];
-        for (int[] p : prerequisites) {
-            Set<Integer> set1 = map.getOrDefault(p[0], new HashSet<>());
-            set1.add(p[1]);
-            map.put(p[0],set1);
+        for (int[] e : prerequisites) {
+            HashSet<Integer> set = map.getOrDefault(e[0], new HashSet<>());
+            set.add(e[1]);
+            map.put(e[0], set);
         }
         for (int i = 0; i < numCourses; i++) {
-            boolean flag = false;
-            if (visited[i] == 0) {
-                flag = dfs(i);
-                if (!flag) return false;
-            } else if (visited[i] == 1) continue;
+            if (visited[i] == 0) if (!dfs(i)) return false;
         }
         return true;
     }
@@ -21,12 +17,13 @@ class Solution {
     public boolean dfs(int i) {
         if (visited[i] == 1) return true;
         if (visited[i] == 2) return false;
+        Set<Integer> neighbors = map.get(i);
         visited[i] = 2;
-        Set<Integer> set = map.get(i);
-        if (set != null)
-            for (int j : set) {
-                if (!dfs(j)) return false;
+        if (neighbors != null) {
+            for (int n : neighbors) {
+                if(!dfs(n)) return false;
             }
+        }
         visited[i] = 1;
         return true;
     }
