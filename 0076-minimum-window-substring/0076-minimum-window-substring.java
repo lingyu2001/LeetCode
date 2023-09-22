1d7b6dddf50eb6bbda9af1c32861d1d12cc6c0d8
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-22 14:17:05 -0700

    Time: 5 ms (82.75%), Space: 43.8 MB (69.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,32 +1,35 @@
 class Solution {
     public String minWindow(String s, String t) {
+        // sliding window
+        // two pointer : left, right;
+        // how to move the sliding window?
+        //      keep moveing pointer right, until # of each character in the window is >= # of each character in t
+        //          how to calculate that? : int[] cntS, int[] cntT
+        //      then calculate the min window
+        //      then move pointer left
         int[] cntS = new int[256];
         int[] cntT = new int[256];
-        int K = 0;
-        int now = 0;
-        int l = 0, r = 0;
-        int ansLeft = -1, ansRight = -1;
+        int now = 0, k = 0;
+        int r = 0, l = 0, minL = -1, minR = -1;
         for (char c : t.toCharArray()) {
             cntT[c]++;
-            if (cntT[c] == 1) K++;
+            if (cntT[c] == 1) k++;
         }
-        for (l = 0; l < s.length(); l++) {
-            while (r < s.length() && now < K) {
+        for (; l < s.length(); l++) {
+            while (r < s.length() && now < k) {
                 cntS[s.charAt(r)]++;
                 if (cntS[s.charAt(r)] == cntT[s.charAt(r)]) now++;
                 r++;
             }
-            if (K == now) {
-                if (ansLeft == -1 || r - l < ansRight - ansLeft) {
-                    ansLeft = l;
-                    ansRight = r;
+            if (now == k) {
+                if (minL == -1 || r - l < minR - minL) {
+                    minL = l;
+                    minR = r;
                 }
             }
-            //move pointer l
             cntS[s.charAt(l)]--;
             if (cntS[s.charAt(l)] < cntT[s.charAt(l)]) now--;
         }
-        if (ansLeft == -1) return "";
-        else return s.substring(ansLeft, ansRight);
+        return minL == -1 ? "" : s.substring(minL, minR);
     }
 }
