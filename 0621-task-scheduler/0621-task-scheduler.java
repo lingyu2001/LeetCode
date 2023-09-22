fd3e3ac39bd7d9a141b22ab3438809724b4d22b3
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-09-22 14:40:46 -0700

    Time: 27 ms (38.69%), Space: 44.6 MB (49.35%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,17 +1,17 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] arr = new int[26];
-        for(char c : tasks) arr[c - 'A']++;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        for (int i : arr) if (i != 0) pq.offer(i);
         int time = 0;
-        while (!q.isEmpty() || !pq.isEmpty()) {
+        int[] arr = new int[26];
+        for (char c : tasks) arr[c - 'A']++;
+        for (int i = 0; i < 26; i++) if (arr[i] > 0) pq.offer(arr[i]);
+        while (!pq.isEmpty() || !q.isEmpty()) {
             time++;
             if (!pq.isEmpty()) {
                 int val = pq.poll();
                 val--;
-                if (val > 0) q.offer(new Pair(val, time + n));
+                if (val >= 1)  q.offer(new Pair(val, time + n));
             }
             if (!q.isEmpty() && q.peek().getValue() == time) {
                 pq.offer(q.poll().getKey());
