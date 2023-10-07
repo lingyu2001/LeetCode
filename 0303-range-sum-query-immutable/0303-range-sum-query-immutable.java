6a6a08004975d6c1ebdede2906fd668f58fec846
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-07 08:16:39 -0700

    Time: 42 ms (29.34%), Space: 48.6 MB (55.47%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,16 +1,15 @@
 class NumArray {
-
     int[] nums;
     public NumArray(int[] nums) {
         this.nums = nums;
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
-        }
     }
     
     public int sumRange(int left, int right) {
-        if (left == 0) return nums[right];
-        return nums[right] - nums[left - 1];
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+        return sum;
     }
 }
 
