13a296429e7d42fc016ff259965b6797b60eb7e5
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-11 06:35:38 -0700

    Time: 0 ms (100%), Space: 43.6 MB (50.23%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,18 +1,36 @@
 class Solution {
+    // public int removeDuplicates(int[] nums) {
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     int s = 0, f = 0;
+    //     while (f < nums.length) {
+    //         int cnt = map.getOrDefault(nums[f], 0);
+    //         if (cnt >= 2) {
+    //             f++;
+    //         } else {
+    //             map.put(nums[f], cnt + 1);
+    //             nums[s] = nums[f];
+    //             s++;
+    //             f++;
+    //         }
+    //     }
+    //     return s;
+    // }
+
     public int removeDuplicates(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         int s = 0, f = 0;
+        int cnt = 0;
         while (f < nums.length) {
-            int cnt = map.getOrDefault(nums[f], 0);
-            if (cnt >= 2) {
-                f++;
-            } else {
-                map.put(nums[f], cnt + 1);
-                nums[s] = nums[f];
+            if (nums[s] != nums[f]) {
                 s++;
-                f++;
+                nums[s] = nums[f];
+            } else if (s < f && cnt < 2) {
+                s++;
+                nums[s] = nums[f];
             }
+            f++;
+            cnt++;
+            if (f < nums.length && nums[f] != nums[f - 1]) cnt = 0;
         }
-        return s;
+        return s + 1;
     }
 }
