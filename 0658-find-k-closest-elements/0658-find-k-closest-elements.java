b661a467212a685c74299a5f059e610ea8c48127
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-10 21:36:17 -0700

    Time: 5 ms (53.33%), Space: 45.4 MB (7.81%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                l = mid;
+                break;
+            }else if (arr[mid] > x) r = mid - 1;
+            else l = mid + 1;
+        }
+        // System.out.println(l + "," + r);
+        int left = l - 1, right = l;
+        LinkedList<Integer> res = new LinkedList<>();
+        while (right - left - 1 < k) {
+            if (left == -1) {
+                res.addLast(arr[right]);
+                right++;
+            } else if (right == arr.length) {
+                res.addFirst(arr[left]);
+                left--;
+            } else if (x - arr[left] > arr[right] - x) {
+                res.addLast(arr[right]);
+                right++;
+            } else {
+                res.addFirst(arr[left]);
+                left--;
+            }
+        }
+        return res;
+    }
+}
