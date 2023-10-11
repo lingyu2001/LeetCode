49dfa37d80d0120a1011b09fc8ea9a214534f985
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-10 20:32:33 -0700

    Time: 2 ms (86.39%), Space: 45.1 MB (84.56%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,10 +1,15 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0, right = numbers.length - 1;
-        while (left < right) {
-            if (numbers[left] + numbers[right] == target) return new int[]{left + 1, right + 1};
-            else if (numbers[left] + numbers[right] < target) left++;
-            else right--;
+        int l = 0, r = numbers.length - 1;
+        while (l <= r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) {
+                return new int[]{l + 1, r + 1};
+            } else if (sum > target) {
+                r--;
+            } else {
+                l++;
+            }
         }
         return null;
     }
