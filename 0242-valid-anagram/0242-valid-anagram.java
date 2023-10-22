27cac046ff89b61b77e1e8db668b59daaf0a69fb
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-22 16:58:38 -0700

    Time: 5 ms (54.44%), Space: 42.3 MB (71.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        int[] arr = new int[256];
         if (s.length() != t.length()) return false;
-        int[] arr = new int[26];
+        // keep track on the number of characters in s
         for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+            arr[s.charAt(i)]++;
         }
-        for (int i = 0; i < 26; i++) {
-            if (arr[i] != 0) return false;
+        for (int i = 0; i < t.length(); i++) {
+            arr[t.charAt(i)]--;
+            if (arr[t.charAt(i)] < 0) return false;
         }
         return true;
     }
