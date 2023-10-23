62a7df918e4186bc86efa3681d62776bbbc95054
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-23 07:25:56 -0700

    Time: 2 ms (99.34%), Space: 41.6 MB (93.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,31 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
+        // 1. trim
         s = s.trim();
+        // 2. convert to lowercase
+        s = s.toLowerCase();
+        // 3. two pointers, one for the front, one for the back
+        //    while left <= right
+        //    when moving pointers, if it is non-alphanumeric, then skip it, else the pointer stops
+        //    when both pointers stop, compare the two characters, if they are the same, => continue; else return false;
+        // 4. end while => return true;
         int left = 0, right = s.length() - 1;
-        while (left < right) {
-            while (left < right && !isAlphaNum(s.charAt(left))) left++;
-            while (left < right && !isAlphaNum(s.charAt(right))) right--;
-            // System.out.println(left + ":" + s.charAt(left) + " " + right + ":" + s.charAt(right));
-            if (left < right && s.charAt(left) != s.charAt(right)) {
-                return false;
+        while (left <= right) {
+            while (left <= right && !isAlphaNumeric(s, left)) {
+                left++;
             }
+            while (left <= right && !isAlphaNumeric(s, right)) {
+                right--;
+            }
+            // System.out.println(s.substring(left, left + 1) + ' ' + s.substring(right, right + 1));
+            if (left <= right && s.charAt(left) != s.charAt(right)) return false;
             left++;
             right--;
         }
         return true;
     }
 
-    public boolean isAlphaNum(char c) {
-        if (
-            (c >= '0' && c <='9') ||
-           (c >= 'a' && c <= 'z'))
-        return true;
-        else return false;
+    public boolean isAlphaNumeric(String s, int p) {
+        return (s.charAt(p) >= 'a' && s.charAt(p) <= 'z') || (s.charAt(p) >= '0' && s.charAt(p) <= '9');
     }
 }
