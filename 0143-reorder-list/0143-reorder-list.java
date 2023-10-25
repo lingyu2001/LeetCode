e6092285b0ab657b862743bbc72f8fab7b0a384f
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-25 15:08:25 -0700

    Time: 2 ms (89.82%), Space: 45.4 MB (95.7%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,19 +10,40 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode curr = head;
-        while (curr != null && curr.next != null) {
-            ListNode next = curr.next;
-            ListNode pre = curr;
-            while (pre.next.next != null) {
-                pre = pre.next;
-            } 
-            if (pre == curr) return ;
-            ListNode last = pre.next;
-            curr.next = last;
-            pre.next = null;
-            last.next = next;
-            curr = next;
+        if (head == null || head.next == null) return;
+        ListNode fast = head.next, slow = head;
+        // find the middle
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // reverse the later half linkedlist
+        ListNode p = slow.next;
+        slow.next = null;
+        p = reverse(p);
+        // merge
+        ListNode first = head;
+        ListNode second = p;
+        while (second != null) {
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+            first.next = second;
+            second.next = t1;
+            first = t1;
+            second = t2;
         }
     }
+    
+    public ListNode reverse(ListNode head){
+        if (head == null) return null;
+        ListNode cur = head,prev = null,next = head.next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+    
 }
