190fb8d7686f501290b2a35f42b9c972179b046a
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-25 14:25:51 -0700

    Time: 0 ms (100%), Space: 41.3 MB (41.01%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,37 +10,30 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // corner case : len(list1) == 0 or len(list2) == 0
-        // iterate list1 and list2, p1 points to nodes in list1, p2 points to nodes in list2
-        // if (p1.val < p2.val) then 
-        // else t= p1; p1 = p1.next; move t after p2, then let p2 = t;
-        if (list1 == null) return list2;
-        else if (list2 == null) return list1;
         ListNode p1 = list1, p2 = list2;
-        if (list1.val < list2.val) {
-            p2 = list1;
-            p1 = list2;
-        } else {
-            p1 = list1;
-            p2 = list2;
-        }
-        ListNode head = p2;
-        while (p1 != null && p2 != null && p2.next != null) {
-            if (p1.val <= p2.next.val) {
-                ListNode t = p1;
-                p1 = p1.next;
-                t.next = p2.next;
-                p2.next = t;
-                p2 = t;
+        ListNode dummy = new ListNode(-101), p = dummy;
+        while (p1 != null || p2 != null) {
+            if (p1 != null && p2 != null) {
+                if (p1.val <= p2.val) {
+                    p.next = p1;
+                    p1 = p1.next;
+                    p.next.next = null;
+                    p = p.next;
+                } else {
+                    p.next = p2;
+                    p2 = p2.next;
+                    p.next.next = null;
+                    p = p.next;
+                }
+            } else if (p1 != null) {
+                p.next = p1;
+                break;
             } else {
-                p2 = p2.next;
+                // p2 != null
+                p.next = p2;
+                break;
             }
         }
-        if (p1 != null) {
-            p2 = head;
-            while (p2.next != null) p2 = p2.next;
-            p2.next = p1;
-        }
-        return head;
+        return dummy.next;
     }
 }
