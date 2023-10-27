ebbf1740cd910ad4ffc531df2fb72a08fe2162bf
Lingyu LI <ashley.lingyu.li@gmail.com>
2023-10-27 16:29:09 -0700

    Time: 5 ms (54.32%), Space: 44.3 MB (92.82%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,36 +1,47 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
-    int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
     int row, col;
+    int[][] h;
+    boolean[][] pacific = new boolean[row][col];
+    boolean[][] atlantic = new boolean[row][col];
+    int[][] dirs = new int[][]{{0,1}, {1,0}, {-1,0}, {0, -1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        row = heights.length;
-        col = heights[0].length;
-        boolean[][] pacific = new boolean[row][col];
-        boolean[][] atlantic = new boolean[row][col];
+        h = heights;
+        row = h.length;
+        if (row == 0) return res; 
+        col = h[0].length;
+        pacific = new boolean[row][col];
+        atlantic = new boolean[row][col];
         for (int i = 0; i < col; i++) {
-            dfs(heights, 0, i, Integer.MIN_VALUE, pacific);
-            dfs(heights, row - 1, i, Integer.MIN_VALUE, atlantic);
+            helper(0, i, pacific, Integer.MIN_VALUE);
+            helper(row - 1, i, atlantic, Integer.MIN_VALUE);
+        }
+        for (int j = 0; j < row; j++) {
+            helper(j, 0, pacific, Integer.MIN_VALUE);
+            helper(j, col - 1, atlantic, Integer.MIN_VALUE);
         }
         for (int i = 0; i < row; i++) {
-            dfs(heights, i, 0, Integer.MIN_VALUE, pacific);
-            dfs(heights, i, col - 1, Integer.MIN_VALUE, atlantic);
-        }
-        
-        for (int i = 0 ; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (pacific[i][j] && atlantic[i][j])                         
-                    res.add(Arrays.asList(i,j));
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(i);
+                    list.add(j);
+                    res.add(new ArrayList<>(list));
+                }
             }
         }
         return res;
     }
-    
-    public void dfs(int[][] heights, int i, int j, int prev, boolean[][] ocean) {
-        if (i < 0 || j >= col || i >= row || j < 0) return;
-        if (heights[i][j] < prev || ocean[i][j]) return;
+
+    public void helper(int i, int j, boolean[][] ocean, int val) {
+        if (i < 0 || j < 0 || i >= row || j >= col || val > h[i][j] || ocean[i][j]) {
+            return;
+        }
         ocean[i][j] = true;
-        for (int[] d : dir) {
-            dfs (heights, i + d[0], j + d[1], heights[i][j], ocean);
+        for (int[] d : dirs) {
+            int newi = i + d[0];
+            int newj = j + d[1];
+            helper(newi, newj, ocean, h[i][j]);
         }
     }
 }
