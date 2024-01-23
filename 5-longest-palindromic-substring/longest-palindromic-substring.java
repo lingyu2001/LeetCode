a74d2252501529124f8b38e3a08c641def1d5892
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-01-23 12:29:35 -0800

    Time: 19 ms (69.10%) | Memory: 45.1 MB (35.42%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,18 +3,19 @@ class Solution {
         String res = "";
         for (int i = 0; i < s.length(); i++) {
             String s1 = helper (s, i, i);
-            String s2 = helper (s, i, i + 1);
-            res = s1.length() > res.length() ? s1 : res;
-            res = s2.length() > res.length() ? s2 : res;
+            String s2 = helper(s, i, i + 1);
+            res = res.length() > s1.length()? res : s1;
+            res = res.length() > s2.length()? res : s2;
         }
         return res;
     }
 
-    public String helper (String s, int le, int ri) {
-        while (le >= 0 && ri < s.length() && s.charAt(le) == s.charAt(ri)) {
-            le--;
-            ri++;
+    public String helper(String s, int i, int j) {
+        int lo = i, hi = j;
+        while (lo >=0 && hi < s.length() && s.charAt(lo) == s.charAt(hi)) {
+            lo--;
+            hi++;
         }
-        return s.substring(le + 1, ri);
+        return s.substring(lo + 1, hi);
     }
 }
