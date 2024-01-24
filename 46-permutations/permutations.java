144732a54fc5062e9e79927e6fe636f692e13e75
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-01-24 10:26:21 -0800

    Time: 1 ms (95.76%) | Memory: 44.5 MB (36.40%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,25 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
-    LinkedList<Integer> path = new LinkedList<>();
+    List<Integer> path = new LinkedList<>();
     boolean[] used;
     public List<List<Integer>> permute(int[] nums) {
         used = new boolean[nums.length];
-        helper(nums);
+        helper (nums);
         return res;
     }
 
-    public void helper (int[] nums) {
+    public void helper(int[] nums) {
         if (path.size() == nums.length) {
-            res.add(new LinkedList<>(path));
+            res.add(new ArrayList<>(path));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
             if (used[i]) continue;
-            used[i] = true;
             path.add(nums[i]);
+            used[i] = true;
             helper(nums);
-            path.removeLast();
             used[i] = false;
+            path.removeLast();
         }
     }
 }
