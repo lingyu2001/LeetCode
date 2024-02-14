e94300f74d58f41333a798919421917524d0f6f8
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-02-13 18:21:21 -0800

    Time: 5 ms (99.43%) | Memory: 47.9 MB (27.43%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,50 +1,43 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // create a map<encode, list<string>>
-        // itertate the string array, for each str in the array, if the encode of the str
-        // exists in the map, then we add the str to the corresponding list; else we add the encode of the str and 
-        // a list which contains the str to the list
-        // return the values of the map
-        // time complexity is O(kN)
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String s : strs) {
-            String encodingStr = encode(s);
-            if (map.containsKey(encodingStr)) {
-                List<String> list = map.get(encodingStr);
-                list.add(s);
-                map.put(encodingStr, list);
-            } else {
-                List<String> list = new ArrayList<>();
-                list.add(s);
-                map.put(encodingStr, list);
-            }
-        }
+        // hashmap<string, List<String>>
+        // 1. first, iterate each word in the strs array,
+        // for each word,  
+            // 1.1 calculate the encode string of the word. 
+            // 1.2 check if the encode is in the hashmap table
+            // => yes, add the word to the corresponding list
+            // => no, we create a new list, add the encode and the list to the hashmap table
+        // 2. create the result => iterate each value in the hashmap and add them to the result list
+        // 3. return the result
+
+        // how to encode the word? => count the frequency of each character and convert it to the string
         List<List<String>> res = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String w : strs) {
+            String encode = encoder(w);
+            // check if the encode is in the hashmap table
+            List<String> list;
+            if (map.containsKey(encode)) {
+                // yes
+                list = map.get(encode);
+            } else {
+                // no
+                list = new ArrayList<>();
+            }
+            list.add(w);
+            map.put(encode, list);
+        }
         for (List<String> list : map.values()) {
             res.add(list);
         }
-        /*
-            Initialization:
-            char c = '\u0000'; initializes c to the null character, which is the first character in the Unicode character set and has a numerical value of 0.
-
-            Increment Operation:
-            c++; increments the value of c. Since c was 0 (null character), incrementing it by 1 will change its value to 1.
-            
-            Output:
-            System.out.println(c); will then print the character that corresponds to the Unicode value 1. This character is a control character in the Unicode set and is typically not visible or represented as a space in most fonts and systems.
-            Thus, the output of this code will be a representation of the character with Unicode value 1, which is usually not visible. In some environments, it might appear as a space or a special symbol, or there might be no visible output at all.
-        */
-        // char c = '\u0000';
-        // c++;
-        // System.out.println(c);
         return res;
     }
 
-    public String encode(String s) {
-        char[] count = new char[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
+    public String encoder(String w) {
+        char[] chars = new char[26];
+        for (char c : w.toCharArray()) {
+            chars[c - 'a']++;
         }
-        return new String(count);
+        return String.valueOf(chars);
     }
 }
