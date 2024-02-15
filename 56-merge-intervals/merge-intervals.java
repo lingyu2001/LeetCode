5d53a1ff828fd84459dbf13ae7eba7e569949e13
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-02-15 12:11:05 -0800

    Time: 8 ms (88.62%) | Memory: 46.7 MB (29.96%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,36 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-// 1. sort the intervals based on the start;
-// 2. iterate each interval
-// 2.1 if the res is empty=> add this interval directly to the res; 
-// 2.2 else=> get the last interval, compare the current interval (starti, endi)to the last interval(startk, endk);  if they are non-overlapping, they add this interval to the res; if they are overlapping, then merge them and add the merged one to the res;
-// 2.3 return res;
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        LinkedList<int[]> res = new LinkedList<>();
-        for (int[] in : intervals) {
-            if (res.size() == 0) {
-                res.add(in);
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> list = new LinkedList<>();
+        list.add(intervals[0]);
+        for (int i = 1 ; i < intervals.length; i++) {
+            int start = list.getLast()[0];
+            int end = list.getLast()[1];
+            if (end < intervals[i][0]) {
+                list.add(intervals[i]);
             } else {
-                int[] last = res.getLast();
-                // non-overlapping
-                if (last[1] < in[0]) {
-                    res.add(in);
-                // overlapping
-                } else {
-                    res.removeLast();
-                    int start = Math.min(last[0], in[0]);
-                    int end = Math.max(last[1], in[1]);
-                    res.add(new int[]{start, end});
-                }
+                list.getLast()[0] = Math.min(start, intervals[i][0]);
+                list.getLast()[1] = Math.max(end, intervals[i][1]);
             }
         }
-        int[][] res2D = new int[res.size()][2];
+        int[][] res = new int[list.size()][2];
         int idx = 0;
-        for (int[] in : res) {
-            res2D[idx][0] = in[0];
-            res2D[idx][1] = in[1];
+        for (int[] in : list) {
+            res[idx][0] = in[0];
+            res[idx][1] = in[1];
             idx++;
         }
-        return res2D;
+        return res;
     }
 }
