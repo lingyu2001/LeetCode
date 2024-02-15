59e96fb3681ebba74f5644c131e0382878fd40f9
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-02-15 12:55:27 -0800

    Time: 3 ms (23.94%) | Memory: 44.6 MB (85.39%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,16 +1,24 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        LinkedList<int[]> list = new LinkedList<>();
+        if (intervals.length == 0) return new int[][] {newInterval};
         int[][] newIns = new int[intervals.length + 1][2];
-        for (int i = 0; i < newIns.length - 1; i++) {
-            newIns[i] = intervals[i];
+        int i = 0;
+        for (i = 0; i < newIns.length - 1; i++) {
+            if (intervals[i][0] < newInterval[0]) {
+                newIns[i] = intervals[i];
+            } else {
+                break;
+            }
         }
-        newIns[newIns.length - 1] = newInterval;
+        newIns[i++] = newInterval;
+        for (int idx = i - 1; idx < intervals.length; idx++) {
+            newIns[i++] = intervals[idx];
+        }
         return merge(newIns);
     }
 
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        // Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
         LinkedList<int[]> list = new LinkedList<>();
         list.add(intervals[0]);
         for (int i = 1 ; i < intervals.length; i++) {
