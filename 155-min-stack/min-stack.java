512c2d175675a9493456438e77a5fc344be825e3
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-02-16 12:20:46 -0800

    Time: 5 ms (48.31%) | Memory: 45.1 MB (34.17%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,34 +1,32 @@
 class MinStack {
     // 1. create 2 stacks
-    Stack<Long> s1;
-    Stack<Long> s2;
-    long min;
+    Stack<Integer> s1;
+    Stack<Integer> s2;
     public MinStack() {
         s1 = new Stack<>();
         s2 = new Stack<>();
-        min = Integer.MAX_VALUE;
     }
     
     public void push(int val) {
-        s1.push((long)val);
-        min = Math.min(min,(long)val);
-        s2.push(min);
+        s1.push(val);
+        if (s2.isEmpty()) {
+            s2.push(val);
+        } else {
+            s2.push(Math.min(s2.peek(), val));
+        }
     }
     
     public void pop() {
         s1.pop();
         s2.pop();
-        if (!s2.isEmpty()) {
-            min = s2.peek();
-        } else min = Integer.MAX_VALUE;
     }
     
     public int top() {
-        return s1.peek().intValue();
+        return s1.peek();
     }
     
     public int getMin() {
-        return s2.peek().intValue();
+        return s2.peek();
     }
 }
 
