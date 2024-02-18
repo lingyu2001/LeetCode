0c06da33dbfbc77dc0e3a6625ec3e24f02963a23
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-02-17 20:52:38 -0800

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.95%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,36 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
         int l = 0, r = nums.length - 1;
-        int minVal = Integer.MAX_VALUE;
-        int minIndex = 0;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < minVal) {
-                minVal = nums[mid];
-                minIndex = mid;
-            }
-            if (nums[mid] <= nums[nums.length - 1]) {
-                // on the right part
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        if (nums[minIndex] > target) return -1;
-        if (nums[nums.length - 1] >= target) {
-            l = minIndex;
-            r = nums.length - 1;
-        } else {
-            l = 0;
-            r = minIndex - 1;
-        }
         while (l <= r) {
             int mid = l + (r - l) / 2;
             if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) {
-                r = mid - 1;
+            // the left part is sorted
+            if (nums[l] < nums[mid]) {
+                if (target >= nums[l] && target <= nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
             } else {
-                l = mid + 1;
+                // The right part is sorted
+                if (mid + 1 < nums.length && nums[mid + 1] <= target && target <= nums[r]) {
+                    // target is in the right part
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
         }
         return -1;
