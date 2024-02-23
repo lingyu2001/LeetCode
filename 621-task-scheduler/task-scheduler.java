6a46dba619517e225e7590f40fe28a38f89e8b23
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-02-22 16:08:59 -0800

    Time: 26 ms (43.53%) | Memory: 45.7 MB (48.85%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,25 +1,29 @@
 class Solution {
-
     public int leastInterval(char[] tasks, int n) {
-        if (n == 0) return tasks.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        int[] arr = new int[26];
-        for (char c : tasks) arr[c - 'A']++;
-        for (int val : arr) if (val > 0) pq.add(val);
         int time = 0;
-
-        while ((!pq.isEmpty() || !q.isEmpty())) {
+        int[] cnt = new int[26];
+        for (char c : tasks) {
+            cnt[c - 'A']++;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        for (int i : cnt) {
+            if (i > 0) pq.add(i);
+        }
+        // q stores the earlist time that tasks are waiting to be scheduled
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        // pq stores the tasks that can be scheduled immediately
+        while (!pq.isEmpty() || !q.isEmpty()) {
             time++;
             if (!pq.isEmpty()) {
                 int val = pq.poll();
                 val--;
-                if (val > 0) q.add(new Pair(val, time + n));
+                if (val > 0) {
+                    q.add(new Pair(val, time + n));
+                }
             }
-
-            if (!q.isEmpty() && q.peek().getValue() == time) pq.add(
-                q.poll().getKey()
-            );
+            if (!q.isEmpty() && q.peek().getValue() == time) {
+                pq.add(q.poll().getKey());
+            }
         }
         return time;
     }
