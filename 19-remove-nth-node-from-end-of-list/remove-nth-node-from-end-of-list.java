5991af7c8987f100236575fe629d013d98d8f5dd
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-08-30 21:21:32 -0700

    Time: 0 ms (100.00%) | Memory: 41.5 MB (76.76%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,25 +10,18 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // method 1 : 1. get the full length of the list l . 2. the last nth node <=> the first l - n + 1
-        // method 2 : create a dummy node ; we have a pointer p1, it points at the nth node in the list(without dummy node). pointer p2 pointed at the head of the list
-                    // and then we keep moving the p1 and p2 at the same time until p2 reaches the null 
-        ListNode p1 = head, dummy = new ListNode(-1, head), p2 = dummy;
-        int cnt = 1;
-        while (true) {
-            if (cnt == n) break;
-            else {
-                p1 = p1.next;
-                cnt++;
-            }
+        ListNode dummy = new ListNode(-1, head);
+        // find n + 1 
+        ListNode p1 = dummy, p2 = dummy;
+        for (int i = 1; i <= n + 1; i++) {
+            p2 = p2.next;
         }
-        while (p1.next != null) {
+        while (p2 != null) {
             p1 = p1.next;
             p2 = p2.next;
         }
-        // p2 points at the node in front of the last nth node
-        ListNode nthNode = p2.next;
-        p2.next = nthNode.next;
+        // Now p1 is pointing at n+1 node from the end
+        p1.next = p1.next.next;
         return dummy.next;
     }
 }
