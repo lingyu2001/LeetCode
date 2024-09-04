7efa8f1d8b3d4918b3e0f029b4f5617d58693aba
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-09-03 19:09:47 -0700

    Time: 0 ms (100.00%) | Memory: 44.6 MB (25.09%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -13,16 +13,18 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode p1 = head, p2 = head;
         while (p2 != null && p2.next != null) {
-            p1 = p1.next;
             p2 = p2.next.next;
+            p1 = p1.next;
             if (p1 == p2) break;
         }
-        if (p2 == null || p2.next == null) return null; // has cycle
+        if (p2 == null || p2.next == null) {
+            return null;
+        } 
         p1 = head;
         while (p1 != p2) {
             p1 = p1.next;
             p2 = p2.next;
         }
-        return p1;
+        return p2;
     }
 }
