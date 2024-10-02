19c4e7d5537aab2358de35f6408f128c2c982e7a
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-01 23:45:06 -0700

    Time: 3 ms (72.90%) | Memory: 42.6 MB (90.75%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,24 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.trim();
         s = s.toLowerCase();
-        int l = 0, r = s.length() - 1;
-        while (l <= r) {
-            while (l <= r && !isAphanumeric(s, l)) {
-                l++;
-            }
-            while (l <= r && !isAphanumeric(s, r)) {
-                r--;
-            }
-            if (l <= r && s.charAt(l) != s.charAt(r)) {
-                return false;
+        int i = 0, j = s.length() - 1;
+        while (i <= j) {
+            while (i <= j && !isAlphanumeric(s, i)) i++;
+            while (i <= j && !isAlphanumeric(s, j)) j--;
+            if (i <= j) {
+                if(s.charAt(i) == s.charAt(j)){
+                    i++;
+                    j--;
+                }
+                else return false;
             } 
-            l++;
-            r--;
         }
         return true;
     }
-    boolean isAphanumeric (String s, int p) {
+
+    public boolean isAlphanumeric(String s, int p) {
         return (s.charAt(p) >= 'a' && s.charAt(p) <= 'z') || (s.charAt(p) >= '0' && s.charAt(p) <= '9');
     }
 }
