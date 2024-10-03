4f81ee9a103fc900c5b2e6dbac304bb3af449e68
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-02 20:17:37 -0700

    Time: 549 ms (54.17%) | Memory: 13 MB (34.41%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,39 +1,38 @@
-from collections import deque
-
 class Solution(object):
     directions = [
         [1, 0], [0, 1], [-1, 0], [0, -1],
         [1, 1], [1, -1], [-1, 1], [-1, -1]
     ]
-
+    m = 0
     def shortestPathBinaryMatrix(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        n = len(grid)
-        
-        if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
+        self.m = len(grid)
+        if grid[0][0] == 1 or grid[self.m - 1][self.m - 1] == 1:
             return -1
+        return self.bfs(grid, 0, 0)
 
-        # BFS initialization
-        queue = deque([(0, 0, 1)])  # (row, column, distance)
+    def bfs(self, grid, i, j) :
+        queue = deque([(i,j,1)])
         visited = set((0, 0))
-
         while queue:
-            x, y, dist = queue.popleft()
-
-            # If we've reached the bottom-right corner, return the distance
-            if x == n - 1 and y == n - 1:
+            x,y,dist = queue.popleft()
+            visited.add((x,y))
+            if x == self.m - 1 and y == self.m - 1:
                 return dist
+            for a,b in self.directions:
+                new_x = x + a
+                new_y = y + b
+                # Check boundaries and obstacles
+                if (0 <= new_x < self.m and 
+                    0 <= new_y < self.m and 
+                    grid[new_x][new_y] == 0 and 
+                    (new_x, new_y) not in visited):
+                    queue.append((new_x, new_y, dist + 1))
+                    visited.add((new_x, new_y))  # Mark as visited
+        return -1
 
-            # Explore all 8 possible directions
-            for dx, dy in self.directions:
-                nx, ny = x + dx, y + dy
-
-                # Check if the new position is valid and not visited
-                if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 0 and (nx, ny) not in visited:
-                    visited.add((nx, ny))
-                    queue.append((nx, ny, dist + 1))
-
-        return -1  # If there is no path
+        
+        
