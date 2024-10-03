ae1d9b4a2f7491df932bbe359326ea05b4fd575b
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-02 23:44:03 -0700

    Time: 751 ms (84.42%) | Memory: 17.1 MB (6.79%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,28 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        res = []
+        res = set()  # Use a set to avoid duplicate triplets
         nums = sorted(nums)
-        for i in range(0, len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1] :
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
+            
             target = -nums[i]
-            # print(target)
             j = i + 1
             k = len(nums) - 1
+            
             while j < k:
-                # print(j, k , nums[j] + nums[k])
-                if nums[j] + nums[k] == target:
-                    res.append([nums[i], nums[j], nums[k]])
-                    while j < k and nums[j + 1] == nums[j]:
+                current_sum = nums[j] + nums[k]
+                if current_sum == target:
+                    res.add((nums[i], nums[j], nums[k]))  # Add the triplet as a tuple to the set
+                    while j < k and nums[j] == nums[j + 1]:
                         j += 1
-                    while j < k and nums[k - 1] == nums[k]:
+                    while j < k and nums[k] == nums[k - 1]:
                         k -= 1
                     j += 1
                     k -= 1
-                elif nums[j] + nums[k] > target:
+                elif current_sum > target:
                     k -= 1
                 else:
                     j += 1
-        return res
-                
+        
+        return [list(triplet) for triplet in res] 
