a72f5ea5315cf415c5d1cb4e16d7952d00055fd9
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-12 10:04:02 -0700

    Time: 74 ms (70.95%) | Memory: 19.8 MB (6.82%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,22 +1,32 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-            lst = list(s)
-            stack = []
-            # iterate every char in the string
-            for idx, c in enumerate(lst):
-                if ord('a') <= ord(c) <= ord('z'):
+        '''
+        1. iterate the string, and remove all unmatched )
+            balance, left_parethesis_seen
+        2. iterate the string, and remove all unmatched (
+        '''
+        lst = list(s)
+        balance = 0
+        matched = 0
+        first_pass = []
+        second_pass = []
+        # remove all unmatched )
+        for c in lst:
+            if c == '(':
+                balance += 1
+            elif c == ')':
+                if balance == 0:
                     continue
-                elif c == "(":
-                    stack.append(idx)
-                else :
-                    if len(stack) == 0:
-                        lst[idx] = ' '
-                    else:
-                        stack.pop()
-            while len(stack) != 0:
-                lst[stack.pop()] = ' '
-            res = ""
-            for c in lst:
-                if c != ' ':
-                    res = res+c
-            return res
+                else:
+                    balance -= 1
+                    matched += 1
+            first_pass.append(c)
+        # remove all unmatched (
+        for c in first_pass:
+            if c == '(':
+                if matched == 0:
+                    continue
+                else:
+                    matched -= 1
+            second_pass.append(c)
+        return "".join(second_pass)
