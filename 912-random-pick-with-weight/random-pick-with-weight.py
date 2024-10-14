0ab458cb660031ee7cbba3555af0b3047039a2b0
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-14 10:52:25 -0700

    Time: 167 ms (44.35%) | Memory: 21.1 MB (59.76%) - LeetSync

---
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,24 @@
+class Solution:
+    def __init__(self, w: List[int]):
+        # calculate preSum array
+        self.preSum = [0]
+        for c in w:
+            self.preSum.append(self.preSum[-1] + c)
+    def pickIndex(self) -> int:
+        # generate a number m between 1 and preSum[-1]
+        num = random.randint(1, self.preSum[-1])
+        # binary search
+        lo = 0
+        hi = len(self.preSum)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if self.preSum[mid] < num:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo - 1
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
