f68e933a6d2070f19c624039502ba0ddff361706
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-14 11:44:16 -0700

    Time: 78 ms (95.11%) | Memory: 22 MB (12.79%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = [c for c in s if c != ' ']
+        sign = "+"
+        s += '+'
+        sign = "+"
+        res = 0
+        num = 0
+        stack = []
+        for c in s:
+            if c.isnumeric():
+                num = num * 10 + int(c)
+            else:
+                if sign == '*':
+                    stack.append(stack.pop() * num)
+                elif sign == '/':
+                    stack.append(math.trunc(stack.pop() / num))
+                elif sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                num = 0
+                sign = c
+        for n in stack:
+            res += n
+        return res
