10f8f55ab0be2fa53695f2cf5614d6bca7167f57
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-14 14:49:45 -0700

    Time: 97 ms (72.99%) | Memory: 19.2 MB (83.80%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,25 +1,25 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s += '+'
+        s = s+'+'
         sign = '+'
         res = 0
-        num = 0
         last_num = 0
+        num = 0
         for c in s:
             if c.isnumeric():
                 num = num * 10 + int(c)
             elif c != ' ':
                 if sign == '*':
-                    last_num = num * last_num
+                    last_num = last_num * num
                 elif sign == '/':
-                    last_num = math.trunc(last_num/num)
+                    last_num = math.trunc(last_num / num)
                 elif sign == '+':
                     res += last_num
                     last_num = num
                 elif sign == '-':
                     res += last_num
                     last_num = -num
-                num = 0
                 sign = c
+                num = 0
         res += last_num
         return res
