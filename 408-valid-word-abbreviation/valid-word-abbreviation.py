8ac792210f32dbfbba7301d2da2f4381e8b13833
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-15 21:31:47 -0700

    Time: 31 ms (82.91%) | Memory: 16.6 MB (18.66%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,21 +1,26 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        ws = [c for c in word if c != ' ']
-        m, n = len(ws), len(abbr)
-        i, j = 0, 0
-        while i < m and j < n:
-            if ws[i] == abbr[j]:
-                i += 1
-                j += 1
-            elif abbr[j] == '0':
-                return False
-            elif abbr[j].isnumeric():
-                k = j
-                while k < n and abbr[k].isnumeric():
-                    k += 1
-                length = int(abbr[j:k])
-                i = i + length
-                j = k
+        # remove all spaces in word
+        word = [w for w in word if w != ' ']
+        p = 0
+        q = 0
+        # compare if word matches abbr
+        while p < len(word) and q < len(abbr):
+            # assume q is pointing at a letter in abbr 
+            if word[p] == abbr[q]:
+                q += 1
+                p += 1
+            # if q is pointing at a digit
+            elif abbr[q].isnumeric():
+                num = 0
+                if abbr[q] == '0':
+                    return False
+                # convert the str to int
+                while q < len(abbr) and abbr[q].isnumeric():
+                    num = num * 10 + int(abbr[q])
+                    q += 1
+                p = p + num
             else:
+                # q and p just dont match
                 return False
-        return i == m and j == n
+        return p == len(word) and q == len(abbr)
