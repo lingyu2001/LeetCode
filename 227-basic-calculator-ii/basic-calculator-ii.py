1d8ed6182442fba1866527c5bc5379fec40b903d
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-16 23:37:15 -0700

    Time: 86 ms (86.61%) | Memory: 19.2 MB (84.01%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,25 +1,28 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s+'+'
-        sign = '+'
+        s = s + "+"
+        sign = "+"
+        cur = 0
+        prev = 0
         res = 0
-        last_num = 0
-        num = 0
         for c in s:
+            # c is digit
             if c.isnumeric():
-                num = num * 10 + int(c)
-            elif c != ' ':
-                if sign == '*':
-                    last_num = last_num * num
-                elif sign == '/':
-                    last_num = math.trunc(last_num / num)
-                elif sign == '+':
-                    res += last_num
-                    last_num = num
-                elif sign == '-':
-                    res += last_num
-                    last_num = -num
-                sign = c
-                num = 0
-        res += last_num
+                # convert the digit to the total num
+                cur = cur * 10 + int(c)
+            else:
+                if c != ' ':
+                    if sign == "+":
+                        res += prev
+                        prev = cur
+                    elif sign == "-":
+                        res += prev
+                        prev = -cur
+                    elif sign == "*":
+                        prev = prev * cur
+                    elif sign == "/":
+                        prev = math.trunc(prev/cur)
+                    cur = 0
+                    sign = c
+        res += prev
         return res
