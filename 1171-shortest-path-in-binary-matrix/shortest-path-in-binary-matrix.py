3df0521dec43fda1ea8549713af0e48cd1db3775
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-17 09:23:22 -0700

    Time: 269 ms (100.00%) | Memory: 12.9 MB (44.64%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,38 +1,31 @@
 class Solution(object):
-    directions = [
-        [1, 0], [0, 1], [-1, 0], [0, -1],
-        [1, 1], [1, -1], [-1, 1], [-1, -1]
-    ]
-    m = 0
     def shortestPathBinaryMatrix(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        self.m = len(grid)
-        if grid[0][0] == 1 or grid[self.m - 1][self.m - 1] == 1:
+        directions = [
+            [1, 0], [0, 1], [-1, 0], [0, -1],
+            [1, 1], [1, -1], [-1, 1], [-1, -1]
+        ]
+        depth = 0
+        n = len(grid)
+        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
-        return self.bfs(grid, 0, 0)
-
-    def bfs(self, grid, i, j) :
-        queue = deque([(i,j,1)])
-        visited = set((0, 0))
+        visited = set()
+        queue = deque()
+        queue.append((0,0))
         while queue:
-            x,y,dist = queue.popleft()
-            visited.add((x,y))
-            if x == self.m - 1 and y == self.m - 1:
-                return dist
-            for a,b in self.directions:
-                new_x = x + a
-                new_y = y + b
-                # Check boundaries and obstacles
-                if (0 <= new_x < self.m and 
-                    0 <= new_y < self.m and 
-                    grid[new_x][new_y] == 0 and 
-                    (new_x, new_y) not in visited):
-                    queue.append((new_x, new_y, dist + 1))
-                    visited.add((new_x, new_y))  # Mark as visited
+            size = len(queue)
+            depth += 1
+            # explore all cells at the current depth
+            for _ in range(size):
+                i,j = queue.popleft()
+                visited.add((i,j))
+                if i == n - 1 and j == n - 1:
+                    return depth
+                for a,b in directions:
+                    newi = i + a
+                    newj = j + b
+                    # validate (newi,newj)
+                    if 0 <= newi < n and 0 <= newj < n and grid[newi][newj] == 0 and (newi, newj) not in visited:
+                        queue.append((newi, newj))
+                        visited.add((newi, newj))
         return -1
-
-        
         
