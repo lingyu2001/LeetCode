5380f10b00c7d2da4b687f89d08225b90cb885a5
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-16 18:23:34 -0700

    Time: 44 ms (13.10%) | Memory: 16.5 MB (51.65%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -6,19 +6,22 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        ma = defaultdict(list)
+        map = defaultdict(list)
         queue = deque()
         if root:
             queue.append((root, 0))
         while queue:
-            size = len(queue)
+            size  = len(queue)
+            # get all nodes in current level
             for _ in range(size):
-                node, num = queue.popleft()
-                ma[num].append(node.val)
+                node, idx = queue.popleft()
+                # add node to map based on idx
+                map[idx].append(node.val)
+                # add left child node and right child node
                 if node.left:
-                    queue.append((node.left, num - 1))
+                    queue.append((node.left, idx - 1))
                 if node.right:
-                    queue.append((node.right, num + 1))
-        keys = list(ma.keys())
+                    queue.append((node.right, idx + 1))
+        keys = list(map.keys())
         keys.sort()
-        return [ma[k] for k in keys]
+        return [map[k] for k in keys]
