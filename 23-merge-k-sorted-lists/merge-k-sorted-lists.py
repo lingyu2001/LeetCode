4c206e1a4183b7a7ca237c553d7355e8be835efb
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-22 19:17:40 -0700

    Time: 14 ms (99.13%) | Memory: 17.4 MB (96.70%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -9,21 +9,26 @@ class Solution(object):
         :type lists: List[Optional[ListNode]]
         :rtype: Optional[ListNode]
         """
+        # nlogk
+        # k
+        # put all elements in the queue
         pq = []
-        head = ListNode(0)
-        cur = head
         for i in lists:
             if i:
                 heapq.heappush(pq, (i.val, i))
-        
-        # get the smallest number in the pq 
-        while pq:
-            sm_val, lst = heapq.heappop(pq)
-            node = lst
-            cur.next = lst
-            cur = cur.next
-            lst = lst.next
-            # check if the lst still has values
+        # lists = [[1,4,5],[1,3,4],[2,6]]
+        # pq:
+        (5,[5])
+        head = pointer = ListNode(0)
+        # get the smallest Listnode each time when poping
+        while pq:  #      (4,[4])
+            val, lst = heapq.heappop((pq))
+            # add the first node of the list to res linkedlist
+            node = lst # [4]
+            lst = lst.next #[]
+            pointer.next = node #[0,1,1,2,3,4,4]
+            pointer = pointer.next # 4, indx=5
             if lst:
                 heapq.heappush(pq, (lst.val, lst))
         return head.next
+        
