a2d494f14c2a08600f80e2995a86574e18cd5c1e
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-29 16:54:55 -0700

    Time: 2162 ms (5.55%) | Memory: 45.4 MB (12.62%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,26 +1,49 @@
+class TrieNode:
+    def __init__(self):
+            self.children = [None] * 10
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, num):
+        node = self.root
+        num_str = str(num)
+        for d in num_str:
+            idx = int(d)
+            if not node.children[idx]:
+                node.children[idx] = TrieNode()
+            node = node.children[idx]
+        
+    def find_longest_prefix(self, num):
+        node = self.root
+        res = 0
+        num_str = str(num)
+        for d in num_str:
+            idx = int(d)
+            if node.children[idx]:
+                res += 1
+                node = node.children[idx]
+            else:
+                return res
+        return res
 class Solution(object):
+
     def longestCommonPrefix(self, arr1, arr2):
         """
         :type arr1: List[int]
         :type arr2: List[int]
         :rtype: int
         """
-        # build prefix set for arr1
-        prefix = set()
-        for n in arr1:
-            while n > 0:
-                prefix.add(n)
-                n = n // 10
-        # find the longest matching prefix in arr2
         res = 0
-        for n in arr2:
-            while n > 0:
-                if n in prefix:
-                    if res < len(str(n)):
-                        res = len(str(n))
-                    break
-                else:
-                    n = n // 10
-        return res
-
+        # insert nums in arr1 to trie
+        trie = Trie()
+        for num in arr1:
+            trie.insert(num)
         
+        # find the longest common prefix in arr2
+        for num in arr2:
+            longest_prefix = trie.find_longest_prefix(num)
+            res = max(res, longest_prefix)
+        return res
+    
+    
