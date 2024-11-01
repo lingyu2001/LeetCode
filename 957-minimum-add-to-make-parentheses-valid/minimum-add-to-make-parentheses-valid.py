4c7b122de0e9ad0574980222eb4fa7d6b329587e
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-01 00:24:07 -0700

    Time: 0 ms (100.00%) | Memory: 11.7 MB (26.78%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,13 +1,20 @@
-class Solution:
-    def minAddToMakeValid(self, s: str) -> int:
+class Solution(object):
+    def minAddToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        left = 0
+        right = 0
         res = 0
-        lst = []
         for c in s:
-            if c == "(":
-                lst.append(c)
-            else:
-                if len(lst) == 0:
-                    res += 1
-                else:
-                    lst.pop()
-        return res+len(lst)
+            if c == '(':
+                left += 1
+            elif c == ')':
+                right += 1
+            if left < right:
+                res += (right - left)
+                left = right = 0
+        if left > right:
+            res += (left - right)
+        return res
