eee29eb659e7352c8783e77d43affd4f67cd566b
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-31 22:29:30 -0700

    Time: 0 ms (100.00%) | Memory: 11.6 MB (66.45%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -4,27 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # 5 3  2 1 4
-        n = len(nums) - 1
-        i = n - 1
-        j = n
-        while i >= 0 and nums[i] >= nums[j] :
-            i -= 1
-            j -= 1
-        if i < 0:
-            nums.reverse()
-        else:
-            val = 101
-            idx = -1
-            for k in range(i + 1, n + 1):
-                if nums[k] > nums[i]:
-                    if nums[k] <= val:
-                        idx = k
-                        val = nums[k]
-            nums[i], nums[idx] = nums[idx], nums[i]
-            i += 1
-            j = n
-            while (i < j):
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
+        """
+        When reading from right to left, 
+        if the sequence is descending, 
+        that part of the sequence represents the highest possible order for those elements (like [3, 2, 1]). 
+        So, to get a larger permutation, we need to find where we can "break" this descending order to make a small increase.
+        """
+        # find the last increasing position
+        i = len(nums) - 2
+        while i >= 0:
+            if nums[i] >= nums[i + 1]:
+                i -= 1
+            else:
+                break
+        if i >= 0:
+            # find the smallest num greater than nums[i]
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
                 j -= 1
+            # swap
+            nums[i], nums[j] = nums[j], nums[i]
+            # reverse
+        m = i + 1 
+        n = len(nums) - 1
+        while m < n:
+            nums[m], nums[n] = nums[n], nums[m]
+            m += 1
+            n -= 1
+            
+
+
