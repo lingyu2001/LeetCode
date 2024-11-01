7a3babf5a38997467496fb93919a78a8d49df869
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-31 23:39:47 -0700

    Time: 4 ms (45.79%) | Memory: 11.7 MB (74.31%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        # replace // ///
+        path = path.replace("//","/")
+        path = path.replace("///", "/")
+        paths = path.split("/")
+        paths = [p for p in paths if p]
+        res = []
+        for content in paths:
+            if content == '.':
+                continue
+            elif content == '..':
+                if len(res) >= 1:
+                    res = res[:-1]
+            else:
+                res.append(content)
+        res_str = ['/' + c for c in res]
+        return "".join(res_str) if len(res_str) > 0 else "/"
