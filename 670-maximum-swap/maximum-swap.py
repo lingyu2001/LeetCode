dd8879bb8c67f09fca13b3ffc887acd391215e07
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-10-31 18:55:30 -0700

    Time: 0 ms (100.00%) | Memory: 16.5 MB (63.98%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,24 +1,16 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        s = str(num)
-        map = {i:deque() for i in range (0,10)}
-        s = list(s)
-        for idx, c in enumerate(s,0):
-            map[int(c)].append(idx)
-        p = 9
-        for idx, c in enumerate(s, 0):
-            while p >= 0 and len(map[p]) == 0:
-                p -= 1
-            if p < 0:
-                return num
-            if int(c) >= p:
-                map[p].popleft()
-                continue
+       num_str = list(str(num))
+       n = len(num_str)
+       max_right_index = [0] * n
+       max_right_index[n - 1] = n - 1
+       for i in range(n - 2, -1, - 1):
+            if num_str[i] > num_str[max_right_index[i + 1]]:
+                max_right_index[i] = i
             else:
-                swap_idx = map[p].pop()
-                s[swap_idx], s[idx] = s[idx], s[swap_idx]
-                res = ""
-                for c in s:
-                    res += c
-                return int(res)
-        return num
+                max_right_index[i] = max_right_index[i + 1]
+       for i in range(n):
+            if num_str[i] != num_str[max_right_index[i]]:
+                num_str[i], num_str[max_right_index[i]] = num_str[max_right_index[i]], num_str[i]
+                return int("".join(num_str))
+       return int("".join(num_str))
