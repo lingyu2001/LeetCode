ee73c591d2bba81bf3f01db90e1158e6f1056110
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-01 11:37:13 -0700

    Time: 15 ms (97.57%) | Memory: 11.7 MB (48.25%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,28 +1,24 @@
 class Solution(object):
-    res = 0
     def maxUniqueSplit(self, s):
         """
         :type s: str
         :rtype: int
         """
-        split = set()
-        self.helper(s, 0, split)
-        return self.res
-
-    def helper (self, s, start, seen):
-        if start >= len(s):
-            self.res = max(self.res, len(seen))
-            return
+        res = [0]
+        seen = set()
+        self.helper(s, 0, seen, res)
+        return res[0]
         
-        for end in range(start + 1, len(s) + 1):
-            substring = s[start : end]
-            if substring not in seen:
-                seen.add(substring)
-                self.helper(s, end, seen)
-                seen.remove(substring)
-
-
-
-
-
+    def helper(self, s, idx, seen, res):
+        if idx == len(s):
+            res[0] = max(res[0], len(seen))
+            return
+        if len(s) - idx + len(seen) <= res[0]:
+            return 
+        for end in range(idx + 1, len(s) + 1):
+            substr = s[idx:end]
+            if substr not in seen:
+                seen.add(substr)
+                self.helper(s, end, seen, res)
+                seen.remove(substr)
         
