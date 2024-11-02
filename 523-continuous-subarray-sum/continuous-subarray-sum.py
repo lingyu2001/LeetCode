4e197504803167aee61a94f7afcbc614dd0c0faf
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 15:57:52 -0700

    Time: 56 ms (39.26%) | Memory: 36.9 MB (33.60%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,16 +1,12 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
-        if n == 2:
-            return sum(nums) % k == 0
-        preSum = [0 for i in range(n + 1)]
-        for i in range(1, n + 1):
-            preSum[i] = preSum[i - 1] + nums[i - 1]
-        map = {0:0}
-        for i in range (1, n + 1):
-            if  (preSum[i] % k) in map:
-                if i - map[preSum[i] % k] >= 2:
+        presum = 0
+        table = {0:-1}
+        for i, n in enumerate(nums):
+            presum += n
+            if presum % k in table:
+                if i - table[presum % k] >= 2:
                     return True
             else:
-                map[preSum[i] % k] = i
+                table[presum % k] = i
         return False
