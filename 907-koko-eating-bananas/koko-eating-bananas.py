b944cd26c892f73fd68878ec2202419c6fc42448
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 15:00:12 -0700

    Time: 141 ms (82.74%) | Memory: 17.7 MB (97.00%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        lo = 1
+        hi = max(piles)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if self.verify(piles, mid, h):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
+    def verify(self, piles, speed, h):
+        res = 0
+        for i in piles:
+            res += (math.ceil(i / speed))
+        return res <= h
