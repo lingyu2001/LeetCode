5e05cbb6a079f7da7b249dd2bf215a82ee0ef5d7
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-01 20:44:23 -0700

    Time: 0 ms (100.00%) | Memory: 11.6 MB (48.25%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,12 +1,16 @@
-class Solution:
-    def customSortString(self, order: str, s: str) -> str:
-        lst = list(s)
-        lst.sort()
-        res = ""
+class Solution(object):
+    def customSortString(self, order, s):
+        """
+        :type order: str
+        :type s: str
+        :rtype: str
+        """
+        cnter = Counter(s)
+        res = []
         for c in order:
-            while c in lst:
-                res += c
-                lst.remove(c)
-        while len(lst) != 0:
-            res += lst.pop()
-        return res
+            if c in cnter:
+                res += [c] * cnter[c]
+                del cnter[c]
+        for c,f in cnter.items():
+            res += [c] * f
+        return "".join(res)
