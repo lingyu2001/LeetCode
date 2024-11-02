d35d5391d53f4297fa3223e2fa1bdbfcb86930f7
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 00:22:46 -0700

    Time: 49 ms (39.83%) | Memory: 24 MB (34.96%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,17 +1,29 @@
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return None
-        if root == p or root == q:
-            return root
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        if left and right:
-            return root
-        elif left:
-            return left
-        elif right:
-            return right
-        else:
-            return None
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        self.ans = None
+        self.helper(root, p, q)
+        return self.ans
+        
+    def helper(self, node, p ,q):
+        if not node:
+            return False
+        left = self.helper(node.left, p, q)
+        right = self.helper(node.right, p, q)
+        mid = node == p or node == q
+        if mid + left + right >= 2:
+            self.ans = node
+        return mid or left or right
         
