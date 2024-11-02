b83584a24e4359df9ea91b626998e120ccb02f82
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 01:56:17 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (9.23%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,27 +1,16 @@
-class Solution(object):
-    map = {}
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        self.map[0] = 1
-        self.map[1] = x
-        self.helper(x, abs(n))
-        if n >= 0:
-            return self.map[n]
-        else:
-            return 1 / self.map[-n]
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return self.helper(x, n)
     def helper(self, x, n):
-        if n == 0 or n == 1:
-            return
-        if n % 2 == 0:
-            self.helper(x, n / 2)
-            self.map[n] = self.map[n/2] * self.map[n/2]
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1.0 / self.helper(x, -1*n)
+        if n % 2 == 1:
+            return x * self.helper(x * x, (n - 1) // 2)
         else:
-            self.helper(x, (n-1)/2)
-            self.map[n - 1] = self.map[(n-1)/2] * self.map[(n-1)/2]
-            self.map[n] = x * self.map[n - 1]
-            
+            return self.helper(x * x, n // 2)
         
+
+
+
