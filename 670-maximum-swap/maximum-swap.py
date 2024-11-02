4a18e1d905de510e5dceba06aabb7ee891a2abe4
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-01 17:13:45 -0700

    Time: 0 ms (100.00%) | Memory: 11.7 MB (36.25%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        nums = list(str(num))
-        n = len(nums)
-        max_index = n - 1
-        left = -1 
+        # max_val_idx = , left=0, right=1
+
+        num_str = list(str(num))
+        max_val_idx = len(num_str) - 1
+        left = -1
         right = -1
-        for i in range(n - 2, -1, -1):
-            if nums[i] > nums[max_index]:
-                max_index = i
-            elif nums[i] < nums[max_index]:
-                left,right = i, max_index
-        if left != -1:
-            nums[left], nums[right] = nums[right], nums[left]
-            return int("".join(nums))
-        return num
-            
+        for i in range(len(num_str) - 2, -1, -1):
+            if num_str[i] > num_str[max_val_idx]:
+                max_val_idx = i
+            elif num_str[i] < num_str[max_val_idx]:
+                left = i
+                right = max_val_idx
+        num_str[left], num_str[right] = num_str[right], num_str[left]
+        return int("".join(num_str))
