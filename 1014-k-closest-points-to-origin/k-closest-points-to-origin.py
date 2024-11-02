8c0c00cbbfd8b2df8318ef3bb694b37fe40232f5
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 01:21:40 -0700

    Time: 61 ms (65.48%) | Memory: 21.3 MB (95.24%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,18 +1,44 @@
-class Solution(object):
-    def kClosest(self, points, k):
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        list1 = []
-        heapq.heapify(list1)
-        for i,j in points:
-            heapq.heappush(list1, (-(i*i+j*j), i, j))
-            if len(list1) > k:
-                heapq.heappop(list1)
-        res = []
-        for i in range(k):
-            a,b,c = heapq.heappop(list1)
-            res += [[b,c]]
-        return res
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return self.quick_select(points, k)
+    
+    def quick_select(self, points: List[List[int]], k: int) -> List[List[int]]:
+        """Perform the QuickSelect algorithm on the list"""
+        left = 0
+        right = len(points) - 1
+        pivot_idx = len(points)
+        while pivot_idx != k:
+            pivot_idx = self.partition(points, left, right)
+            if pivot_idx > k:
+                right = pivot_idx - 1
+            elif pivot_idx < k:
+                left = pivot_idx
+        # Return the first k elements of the partially sorted list
+        return points[:k]
+    
+    def partition(self, points: List[List[int]], left: int, right: int) -> int:
+        """Partition the list around the pivot value"""
+        pivot = self.choose_pivot(points, left, right)
+        pivot_dist = self.squared_distance(pivot)
+        while left < right:
+            # Iterate through the range and swap elements to make sure
+            # that all points closer than the pivot are to the left
+            if self.squared_distance(points[left]) >= pivot_dist:
+                points[left], points[right] = points[right], points[left]
+                right -= 1
+            else:
+                left += 1
+        
+        # Ensure the left pointer is just past the end of
+        # the left range then return it as the new pivotIndex
+        if self.squared_distance(points[left]) < pivot_dist:
+            left += 1
+        return left
+    
+    def choose_pivot(self, points: List[List[int]], left: int, right: int) -> List[int]:
+        """Choose a pivot element of the list"""
+        return points[left + (right - left) // 2]
+    
+    def squared_distance(self, point: List[int]) -> int:
+        """Calculate and return the squared Euclidean distance."""
+        return point[0] ** 2 + point[1] ** 2
