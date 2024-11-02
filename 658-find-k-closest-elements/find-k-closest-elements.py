8892d760376b30a6858926af060b4bce63889701
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 12:58:25 -0700

    Time: 9 ms (53.09%) | Memory: 18.1 MB (64.40%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left = bisect_left(arr, x) - 1
+        right = left + 1
+        # (), both exclusive
+        while right - left - 1 < k:
+            if left == -1:
+                right += 1
+                continue
+            if right == len(arr) or abs(arr[left] - x) <= abs(arr[right] - x):
+                left -= 1
+            else:
+                right += 1
+        return arr[left + 1: right]
