72cecd1d0e8b5ff7d7c85758ae4d90f205452c77
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 02:33:48 -0700

    Time: 2 ms (93.49%) | Memory: 20.1 MB (96.09%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,34 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        cnt = Counter(nums)
-        freq_list = [[v,k]for k,v in cnt.items()]
-        freq_list.sort(reverse=True)
-        return [k for v,k in freq_list[:k]]
+        self.cnt = Counter(nums)
+        return self.quick_select(list(self.cnt.keys()), len(self.cnt.keys()) - k)
+
+    def quick_select(self, nums, k):
+        left = 0
+        right = len(nums) - 1
+        pivot_idx = len(nums)
+        while pivot_idx != k:
+            pivot_idx = self.partition(nums, left, right)
+            if pivot_idx > k:
+                right = pivot_idx - 1
+            else:
+                left = pivot_idx
+        return nums[k:]
+
+    def partition(self, nums, left, right):
+        pivot_idx = random.randint(left, right)
+        pivot_val = self.cnt[nums[pivot_idx]]
+        while left < right:
+            if self.cnt[nums[left]] < pivot_val:
+                left += 1
+            else:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
+        if self.cnt[nums[left]] < pivot_val:
+            left += 1
+        return left
+
+    
+
+
+        
