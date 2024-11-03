e6f086946f24f41e3a424003af64969ab88403b5
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-03 13:27:35 -0800

    Time: 4 ms (15.47%) | Memory: 16.7 MB (67.62%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,21 +1,17 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
         i = 0
-        j = 0
-        while j < len(chars):
-            cnt = 0
-            k = j
-            while k < len(chars) and chars[k] == chars[j]:
-                k += 1
-                # a, a, b
-            cnt = k - j
-            # move char
-            chars[i] = chars[j]
-            # change it to frequency
-            i += 1
-            if cnt > 1:
-                chars[i: i + len(str(cnt))] = list(str(cnt))
-                i = i + len(str(cnt))
-            j = k
-        return i
-
+        res = 0
+        while i < len(chars):
+            group_length = 1
+            while (i + group_length < len(chars)
+                   and chars[i + group_length] == chars[i]):
+                group_length += 1
+            chars[res] = chars[i]
+            res += 1
+            if group_length > 1:
+                str_repr = str(group_length)
+                chars[res:res+len(str_repr)] = list(str_repr)
+                res += len(str_repr)
+            i += group_length
+        return res
