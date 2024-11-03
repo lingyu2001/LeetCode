34a0b357baa057c298e39dd3cf2658fa1dfaa021
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 23:00:18 -0700

    Time: 233 ms (50.68%) | Memory: 45.7 MB (98.25%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.py
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.py
@@ -1,24 +1,16 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        # Convert the intervals to two events
-        # start as (start, 1) and end as (end + 1, -1)
-        events = []
-
-        for interval in intervals:
-            events.append((interval[0], 1))  # Start event
-            events.append((interval[1] + 1, -1))  # End event (interval[1] + 1)
-
-        # Sort the events first by time, and then by type (1 for start, -1 for end).
-        events.sort(key=lambda x: (x[0], x[1]))
-
-        concurrent_intervals = 0
-        max_concurrent_intervals = 0
-
-        # Sweep through the events
-        for event in events:
-            concurrent_intervals += event[1]  # Track currently active intervals
-            max_concurrent_intervals = max(
-                max_concurrent_intervals, concurrent_intervals
-            )  # Update max
-
-        return max_concurrent_intervals
+        pq = []
+        res = 0
+        intervals.sort()
+        for start, end in intervals:
+            if not pq:
+                heapq.heappush(pq, end)
+            else:
+                if pq[0] >= start:
+                    heapq.heappush(pq, end)
+                else:
+                    heapq.heappop(pq)
+                    heapq.heappush(pq, end)
+            res = max(res, len(pq))
+        return res
