9b9e9814ada24b8b79f4284fc4dfc2aefba96f1d
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 17:23:45 -0700

    Time: 4 ms (52.21%) | Memory: 18.9 MB (98.51%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,20 +1,16 @@
-class Solution(object):
-    def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: int
-        """
-        intervals.sort()
-        l = []
-        heapq.heapify(l)
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        pq = []
         res = 0
-        for i, j in intervals:
-            if len(l) == 0:
-                heapq.heappush(l, j)
+        intervals.sort()
+        for start, end in intervals:
+            if not pq:
+                heapq.heappush(pq, end)
             else:
-                while len(l) > 0 and i >= l[0]:
-                    heapq.heappop(l)
-                heapq.heappush(l, j)
-            res = max(res, len(l))
+                if pq[0] > start:
+                    heapq.heappush(pq, end)
+                else:
+                    heapq.heappop(pq)
+                    heapq.heappush(pq, end)
+            res = max(res, len(pq))
         return res
-
