6a8e4f6568045a60538809c38dfaf329cfd114a3
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-02 21:28:57 -0700

    Time: 0 ms (100.00%) | Memory: 16.7 MB (8.18%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -1,14 +1,15 @@
-class Solution:
-    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        for i in range(m):
-            matrix[i] = matrix[i][::-1]
-        map = defaultdict(list)
-        for i in range(m):
-            for j in range(n):
-                map[i+j].append(matrix[i][j])
-        for v in map.values():
-            if len(set(v)) > 1:
-                return False
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        # Get the number of rows and columns in the matrix
+        num_rows = len(matrix)
+        num_cols = len(matrix[0])
+
+        # Iterate over the matrix starting from the second row and second column
+        for row in range(1, num_rows):
+            for col in range(1, num_cols):
+                # Compare the current element with the element diagonally above and to the left
+                if matrix[row][col] != matrix[row - 1][col - 1]:
+                    # If they are not equal, the matrix is not a Toeplitz matrix
+                    return False
+        # If all diagonally adjacent elements are equal, it's a Toeplitz matrix
         return True
