0cb84f916bc8ae46c35429b3444fe04ef58ca0b1
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-03 19:42:47 -0800

    Time: 21 ms (85.07%) | Memory: 20.4 MB (55.73%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.py
@@ -0,0 +1,42 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def __init__(self):
+        self.visited = set()
+        self.adjacent = defaultdict(list)
+
+    def DFS(self, mergedAccount: List[str], email: str):
+        self.visited.add(email)
+        # Add the email to the current component's emails
+        mergedAccount.append(email)
+        
+        if email not in self.adjacent:
+            return
+        
+        for neighbor in self.adjacent[email]:
+            if neighbor not in self.visited:
+                self.DFS(mergedAccount, neighbor)
+
+    def accountsMerge(self, accountList: List[List[str]]) -> List[List[str]]:
+        # Build the adjacency list
+        for account in accountList:
+            accountFirstEmail = account[1]
+            for email in account[2:]:
+                self.adjacent[accountFirstEmail].append(email)
+                self.adjacent[email].append(accountFirstEmail)
+        
+        # Traverse over all accounts to find connected components
+        mergedAccounts = []
+        for account in accountList:
+            accountName = account[0]
+            accountFirstEmail = account[1]
+            
+            # Perform DFS if email is not visited yet
+            if accountFirstEmail not in self.visited:
+                mergedAccount = [accountName]
+                self.DFS(mergedAccount, accountFirstEmail)
+                mergedAccount[1:] = sorted(mergedAccount[1:])  # Sort emails alphabetically
+                mergedAccounts.append(mergedAccount)
+        
+        return mergedAccounts
