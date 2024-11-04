aa8f0c2cce9ca23612e54d34f93143ae804db615
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-03 22:49:40 -0800

    Time: 295 ms (44.88%) | Memory: 17.9 MB (80.26%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,50 +1,32 @@
-from collections import defaultdict
-class Solution(object):
-    def ladderLength(
-        self, beginWord: str, endWord: str, wordList: List[str]
-    ) -> int:
+from collections import deque, defaultdict
 
-        if (
-            endWord not in wordList
-            or not endWord
-            or not beginWord
-            or not wordList
-        ):
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: list) -> int:
+        # Convert wordList to a set for O(1) lookups
+        wordSet = set(wordList)
+        if endWord not in wordSet:
             return 0
+        
+        # BFS initialization
+        queue = deque([(beginWord, 1)])  # Each element is a tuple (word, depth)
+        visited = set([beginWord])
 
-        # Since all words are of same length.
-        L = len(beginWord)
-
-        # Dictionary to hold combination of words that can be formed,
-        # from any given word. By changing one letter at a time.
-        all_combo_dict = defaultdict(list)
-        for word in wordList:
-            for i in range(L):
-                # Key is the generic word
-                # Value is a list of words which have the same intermediate generic word.
-                all_combo_dict[word[:i] + "*" + word[i + 1 :]].append(word)
-
-        # Queue for BFS
-        queue = collections.deque([(beginWord, 1)])
-        # Visited to make sure we don't repeat processing same word.
-        visited = {beginWord: True}
         while queue:
-            current_word, level = queue.popleft()
-            for i in range(L):
-                # Intermediate words for current word
-                intermediate_word = (
-                    current_word[:i] + "*" + current_word[i + 1 :]
-                )
+            current_word, depth = queue.popleft()
 
-                # Next states are all the words which share the same intermediate state.
-                for word in all_combo_dict[intermediate_word]:
-                    # If at any point if we find what we are looking for
-                    # i.e. the end word - we can return with the answer.
-                    if word == endWord:
-                        return level + 1
-                    # Otherwise, add it to the BFS Queue. Also mark it visited
-                    if word not in visited:
-                        visited[word] = True
-                        queue.append((word, level + 1))
-                all_combo_dict[intermediate_word] = []
+            # Generate all possible one-letter transformations
+            for i in range(len(current_word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    transformed_word = current_word[:i] + c + current_word[i+1:]
+
+                    # Check if the transformed word is the endWord
+                    if transformed_word == endWord:
+                        return depth + 1
+
+                    # If transformed_word is in the wordSet and not visited
+                    if transformed_word in wordSet and transformed_word not in visited:
+                        visited.add(transformed_word)
+                        queue.append((transformed_word, depth + 1))
+        
+        # If we exit the loop without finding endWord
         return 0
