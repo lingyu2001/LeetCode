0cca09dd4f4548ce45b7410dcc67e0ce0c1a1034
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-12 19:58:14 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (32.76%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -1,27 +1,19 @@
 class HitCounter:
-    queue = None
     def __init__(self):
-        self.queue = deque()
-        self.total = 0
+        self.hits = []
 
     def hit(self, timestamp: int) -> None:
-        if not self.queue or self.queue[-1][1] != timestamp:
-            self.queue.append([timestamp, 1])
-        else:
-            self.queue[-1][1] += 1
-        self.total += 1
+        self.hits.append(timestamp)
 
     def getHits(self, timestamp: int) -> int:
-        while self.queue:
-            diff = timestamp - self.queue[0][0]
-            if diff >= 300:
-                self.total -= self.queue.popleft()[1]
+        left, right = 0, len(self.hits)
+        target = timestamp - 300
+        while left < right:
+            mid = (left + right) // 2
+
+            if self.hits[mid] <= target:
+                left = mid + 1
             else:
-                break
-        return self.total
-
-
-# Your HitCounter object will be instantiated and called as such:
-# obj = HitCounter()
-# obj.hit(timestamp)
-# param_2 = obj.getHits(timestamp)
+                right = mid
+        
+        return len(self.hits) - left
