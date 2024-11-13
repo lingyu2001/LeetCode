2e537bbd43ab94ead43eacca485170dccb08bfe3
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-12 19:44:05 -0800

    Time: 0 ms (100.00%) | Memory: 16.8 MB (32.76%) - LeetSync

---
+++ 362-design-hit-counter/design-hit-counter.py
@@ -0,0 +1,22 @@
+class HitCounter:
+    queue = None
+    def __init__(self):
+        self.queue = deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.queue.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        while self.queue:
+            diff = timestamp - self.queue[0]
+            if diff >= 300:
+                self.queue.popleft()
+            else:
+                break
+        return len(self.queue)
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
