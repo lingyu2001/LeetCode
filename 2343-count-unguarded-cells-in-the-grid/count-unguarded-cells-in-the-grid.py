ce9f2ce2b4e787cad381d8c56bd3cb6939b72db8
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-20 21:29:16 -0800

    Time: 3589 ms (20.00%) | Memory: 65.8 MB (28.57%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
@@ -17,22 +17,21 @@ class Solution(object):
         for i,j in walls:
             grid[i][j] = 2
  
-        for i, j in guards:
-            for dx, dy in directions:
-                x, y = i + dx, j + dy
-                while 0 <= x < m and 0 <= y < n and grid[x][y] != 1 and grid[x][y] != 2:
-                    if grid[x][y] == 0:  # Only mark unvisited cells
-                        grid[x][y] = 3  # Guarded
-                    x += dx
-                    y += dy
-
-        # for i in range(m):
-        #     for j in range(n):
-        #         if grid[i][j] == 1:
-        #             for dx, dy in directions:
-        #                 self.dfs(grid, m, n, i ,j, dx, dy)
-        # for k in grid:
-        #     print(k)
+        # for i, j in guards:
+        #     for dx, dy in directions:
+        #         x, y = i + dx, j + dy
+        #         while 0 <= x < m and 0 <= y < n and grid[x][y] != 1 and grid[x][y] != 2:
+        #             if grid[x][y] == 0:  # Only mark unvisited cells
+        #                 grid[x][y] = 3  # Guarded
+        #             x += dx
+        #             y += dy
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    for dx, dy in directions:
+                        self.dfs(grid, m, n, i ,j, dx, dy)
+        for k in grid:
+            print(k)
         res = 0
         for i in range(m):
             for j in range(n):
@@ -40,15 +39,10 @@ class Solution(object):
                     res += 1
         return res
     
-    # def dfs(self, grid, m, n, i, j, dx, dy):
-    #     directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-    #     if 0 <= i < m and 0 <=j < n:
-    #         if grid[i][j] == 0:
-    #             grid[i][j] = 3
-    #         if grid[i][j] == 3:
-    #             self.dfs(grid, m, n, i + dx, j + dy, dx, dy) 
-
-        
-            
-        
-        
+    def dfs(self, grid, m, n, i, j, dx, dy):
+        newi = i + dx
+        newj = j + dy
+        if 0 <= newi < m and 0 <= newj < n:
+            if grid[newi][newj] == 0 or grid[newi][newj] == 3:
+                grid[newi][newj] = 3
+                self.dfs(grid, m, n, newi, newj, dx, dy) 
