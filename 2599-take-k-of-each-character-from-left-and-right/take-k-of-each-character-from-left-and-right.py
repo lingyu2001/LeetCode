19cc76455040083bc95e60e29caa5a9ca561a45d
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-21 14:14:07 -0800

    Time: 439 ms (25.08%) | Memory: 15.5 MB (40.32%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -5,21 +5,20 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # cnt = Counter(s)
-        cnt = [0] * 3
-        for c in s:
-            cnt[ord(c) - ord("a")] += 1
-        # if k == 0:
-        #     return 0
+        cnt = Counter(s)
+        # cnt = [0] * 3
+        # for c in s:
+        #     cnt[ord(c) - ord("a")] += 1
+        if k == 0:
+            return 0
         if len(cnt) < 3:
             return -1
-        # for _,v in cnt.items():
-        #     if v < k:
-        #         return -1
-        for i in range(3):
-            if cnt[i] < k:
+        for _,v in cnt.items():
+            if v < k:
                 return -1
-
+        # for i in range(3):
+        #     if cnt[i] < k:
+        #         return -1
         window = [0] * 3
         l = 0
         max_window = 0
@@ -28,9 +27,9 @@ class Solution(object):
             window[ord(s[r]) - ord('a')] += 1
             # shrink
             while l <= r and  (
-                cnt[0] - window[0] < k or 
-                cnt[1] - window[1] < k or 
-                cnt[2] - window[2] < k
+                cnt['a'] - window[0] < k or 
+                cnt['b'] - window[1] < k or 
+                cnt['c'] - window[2] < k
             ):
                 window[ord(s[l]) - ord('a')] -= 1
                 l += 1
