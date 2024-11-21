e40c7dbaf1efe82eb9a08205c19ee39d4d6d9a32
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-21 14:03:34 -0800

    Time: 442 ms (25.08%) | Memory: 15.4 MB (40.32%) - LeetSync

---
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def takeCharacters(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        cnt = Counter(s)
+        if k == 0:
+            return 0
+        if len(cnt) < 3:
+            return -1
+        for _,v in cnt.items():
+            if v < k:
+                return -1
+        window = [0] * 3
+        l = 0
+        max_window = 0
+        for r in range(len(s)):
+            # add to window
+            window[ord(s[r]) - ord('a')] += 1
+            # shrink
+            while l <= r and  (
+                cnt['a'] - window[0] < k or 
+                cnt['b'] - window[1] < k or 
+                cnt['c'] - window[2] < k
+            ):
+                window[ord(s[l]) - ord('a')] -= 1
+                l += 1
+            max_window = max(max_window, r - l + 1)
+        return len(s) - max_window
+
+
+
+
+        
