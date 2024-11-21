315f5dd773d5ea7f94adaafc2e41d1db1b784ff4
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-21 14:11:12 -0800

    Time: 211 ms (70.49%) | Memory: 17.2 MB (69.28%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -1,36 +1,33 @@
-class Solution(object):
-    def takeCharacters(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        cnt = Counter(s)
-        if k == 0:
-            return 0
-        if len(cnt) < 3:
-            return -1
-        for _,v in cnt.items():
-            if v < k:
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        count = [0] * 3
+        n = len(s)
+
+        # Count total occurrences
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+
+        # Check if we have enough characters
+        for i in range(3):
+            if count[i] < k:
                 return -1
+
         window = [0] * 3
-        l = 0
-        max_window = 0
-        for r in range(len(s)):
-            # add to window
-            window[ord(s[r]) - ord('a')] += 1
-            # shrink
-            while l <= r and  (
-                cnt['a'] - window[0] < k or 
-                cnt['b'] - window[1] < k or 
-                cnt['c'] - window[2] < k
+        left, max_window = 0, 0
+
+        # Find the longest window that leaves k of each character outside
+        for right in range(n):
+            window[ord(s[right]) - ord("a")] += 1
+
+            # Shrink window if we take too many characters
+            while left <= right and (
+                count[0] - window[0] < k
+                or count[1] - window[1] < k
+                or count[2] - window[2] < k
             ):
-                window[ord(s[l]) - ord('a')] -= 1
-                l += 1
-            max_window = max(max_window, r - l + 1)
-        return len(s) - max_window
+                window[ord(s[left]) - ord("a")] -= 1
+                left += 1
 
+            max_window = max(max_window, right - left + 1)
 
-
-
-        
+        return n - max_window
