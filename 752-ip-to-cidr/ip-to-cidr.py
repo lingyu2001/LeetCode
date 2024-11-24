c40bb7bf282d74e72eca46a8ddca8787b8469586
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-23 18:55:57 -0800

    Time: 3 ms (43.10%) | Memory: 16.9 MB (12.95%) - LeetSync

--- 752-ip-to-cidr/ip-to-cidr.py
+++ 752-ip-to-cidr/ip-to-cidr.py
@@ -5,17 +5,23 @@ class Solution:
         def bin_to_ip(b):
             return ".".join([str(int(b[i:i+8],2)) for i in range(0, 32, 8)])
 
-
         ans = []
         cur_ip = ip_to_bin(ip)
+
         while n > 0:
             last_one = 31
-            while last_one> -1 and cur_ip[last_one] == '0':
+            while last_one >= 0 and cur_ip[last_one] == '0':
                 last_one -= 1
-            trailing_zero = 31 - last_one
-            while n - (2 ** trailing_zero) < 0:
-                trailing_zero -= 1
-            ans.append(f"{bin_to_ip(cur_ip)}/{32 - trailing_zero}")
-            n -= 2 ** trailing_zero
-            cur_ip = "{:032b}".format(int(cur_ip, 2) + 2 ** trailing_zero)
+            trail_zero = 31 - last_one
+            # fix trail_zero
+            while n < 2 ** trail_zero:
+                trail_zero -= 1
+            # update cur_ip, ans, n
+            ans.append(f"{bin_to_ip(cur_ip)}/{32-trail_zero}")
+            n -= 2 ** trail_zero
+            cur_ip = "{:032b}".format(int(cur_ip,2) + 2 ** trail_zero)
         return ans
+
+            #ans.append(f"{bin_to_ip(cur_ip)}/{32 - trailing_zero}")
+            # n -= 2 ** trailing_zero
+            # cur_ip = "{:032b}".format(int(cur_ip, 2) + 2 ** trailing_zero)
