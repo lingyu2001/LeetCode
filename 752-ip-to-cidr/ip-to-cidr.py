39120ed4d6225b8f27c0726cc478b8b4b2f532fd
Lingyu LI <ashley.lingyu.li@gmail.com>
2024-11-23 19:20:08 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (57.75%) - LeetSync

--- 752-ip-to-cidr/ip-to-cidr.py
+++ 752-ip-to-cidr/ip-to-cidr.py
@@ -1,27 +1,22 @@
 class Solution:
     def ipToCIDR(self, ip: str, n: int) -> List[str]:
-        def ip_to_bin(ip):
-            return "".join(["{:08b}".format(i) for i in map(int, ip.split("."))])
-        def bin_to_ip(b):
-            return ".".join([str(int(b[i:i+8],2)) for i in range(0, 32, 8)])
-
-        ans = []
-        cur_ip = ip_to_bin(ip)
-
+        def ip_to_num(ip):
+            components = map(int, ip.split('.'))
+            return (next(components) << 24) + (next(components) << 16) + (next(components) << 8) + next(components)
+        def num_to_ip(num):
+            mask = (1 << 8) - 1
+            return f'{(num>>24) & mask}.{(num>>16) & mask}.{(num>>8) & mask}.{num & mask}'
+        res = []
+        from math import log2
+        num = ip_to_num(ip)
         while n > 0:
-            last_one = 31
-            while last_one >= 0 and cur_ip[last_one] == '0':
-                last_one -= 1
-            trail_zero = 31 - last_one
-            # fix trail_zero
-            while n < 2 ** trail_zero:
-                trail_zero -= 1
-            # update cur_ip, ans, n
-            ans.append(f"{bin_to_ip(cur_ip)}/{32-trail_zero}")
-            n -= 2 ** trail_zero
-            cur_ip = "{:032b}".format(int(cur_ip,2) + 2 ** trail_zero)
-        return ans
-
-            #ans.append(f"{bin_to_ip(cur_ip)}/{32 - trailing_zero}")
-            # n -= 2 ** trailing_zero
-            # cur_ip = "{:032b}".format(int(cur_ip, 2) + 2 ** trailing_zero)
+            if num == 0:
+                low_bit = 1 << 32
+            else:
+                low_bit = num & (-num)
+            while low_bit > n:
+                low_bit >>= 1
+            n -= low_bit
+            res.append(f'{num_to_ip(num)}/{32 - int(log2(low_bit))}')
+            num += low_bit
+        return res
